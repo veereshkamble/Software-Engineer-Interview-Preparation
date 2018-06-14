8bf9c06abf2c077146c1f8e206bff9627b605011
Veeresh Kamble <veereshkamble7@gmail.com>
2018-06-13 17:41:01 -0700

    Learning Arrays

---
+++ Hello World/src/Arrays.java
@@ -0,0 +1,19 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        numbers[0] = 31;
+        numbers[1] = 45;
+        numbers[2] = 22;
+        numbers[3] = 98;
+        numbers[4] = 10;
+
+        int[] numbers2 = {31, 45, 22, 98, 10};
+
+        String[] myFavoritePlayers = {"Ronaldo", "Messi", "Neymar"};
+        System.out.println(("Index 0: " + myFavoritePlayers[0]));
+        System.out.println(myFavoritePlayers.length);
+
+    }
+}
