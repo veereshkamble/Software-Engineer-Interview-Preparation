b0d4392024f730f8b723bb437c3b56a3dfea9e25
Veeresh Kamble <veereshkamble7@gmail.com>
2018-06-18 15:33:03 -0700

    Weekend break, now starting with selection sort

---
+++ Sorting/src/SelectionSOrt.java
@@ -0,0 +1,32 @@
+public class SelectionSOrt {
+
+    public static void selectionSort(int[] array) {
+        for (int i=0; i<array.length-1; i++) {
+            int minIndex = i;
+            for (int j=i+1; j<array.length; j++) {
+                if(array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<array.length; i++)
+            sb.append(array[i]).append(",");
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args){
+        int[] array = new int[] {7, 5, 9, 2, 4, 1, 3, 6, 8};
+        System.out.println("Before Sort");
+        printArray(array);
+        selectionSort(array);
+        System.out.println("After Sort");
+        printArray(array);
+    }
+}
