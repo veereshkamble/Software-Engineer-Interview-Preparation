5bf6efc739f0a536118d602581f030f2a93b6d89
Veeresh Kamble <veereshkamble7@gmail.com>
2018-06-22 17:43:02 -0700

    deleting nodes in a tree

--- Data Structures/src/Trees/BinarySearchTree.java
+++ Data Structures/src/Trees/BinarySearchTree.java
@@ -19,6 +19,67 @@ public class BinarySearchTree {
     }
 
     public void delete(Integer data) {
+        TreeNode current = this.root;
+        TreeNode parent = this.root;
+        boolean isLeftChild = false;
+
+        if(current == null)
+            return;
+
+        while(current != null && current.getData() != data) {
+            parent = current;
+
+            if(data < current.getData()) {
+                current = current.getLeftChild();
+                isLeftChild = true;
+            } else {
+                current = current.getRightChild();
+                isLeftChild = false;
+            }
+        }
+
+        if (current == null)
+            return;
+
+        if(current.getLeftChild() == null && current.getRightChild() == null) {
+            if (current == root) {
+                root = null;
+            } else {
+                if (isLeftChild)
+                    parent.setLeftChild(null);
+                else
+                    parent.setRightChild(null);
+            }
+        }
+        else if (current.getRightChild() == null) {
+            if (current == root) {
+                root = current.getLeftChild();
+            } else if(isLeftChild) {
+                parent.setLeftChild(current.getLeftChild());
+            } else {
+                parent.setRightChild(current.getLeftChild());
+            }
+        }
+        else if (current.getLeftChild() == null) {
+            if (current == root) {
+                root = current.getRightChild();
+            } else if (isLeftChild) {
+                parent.setLeftChild(current.getRightChild());
+            } else {
+                parent.setRightChild(current.getRightChild());
+            }
+        }
+
+        if(current.getLeftChild() != null) {
+            parent.setLeftChild(current.getLeftChild());
+        }
+
+        if(current.getRightChild() != null) {
+            parent.setRightChild(current.getRightChild());
+        }
+
+
 
     }
 }
+
