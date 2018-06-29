b1134c04cc3d14316764c51954f5e653af3747a0
Veeresh Kamble <veereshkamble7@gmail.com>
2018-06-28 18:51:59 -0700

    Dominant Index

---
+++ Leetcode/src/ArraysAndStrings/DominantIndex.java
@@ -0,0 +1,34 @@
+package ArraysAndStrings;
+
+public class DominantIndex {
+    public int dominantIndex(int[] nums) {
+        if(nums.length == 0) return -1;
+
+        if(nums.length == 1) return 0;
+
+        int secondLargest = 0;
+        int largest = 0;
+        int index = 0;
+
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] >= largest) {
+                secondLargest = largest;
+                largest = nums[i];
+                index = i;
+            } else if (nums[i] != largest && nums[i] >= secondLargest) {
+                secondLargest = nums[i];
+            }
+        }
+
+        if(largest >= 2*secondLargest) {
+            return index;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {3, 1, 0, 4, 10, 5};
+        DominantIndex dominantIndex = new DominantIndex();
+        System.out.println(dominantIndex.dominantIndex(nums));
+    }
+}
