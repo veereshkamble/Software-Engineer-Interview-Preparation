80dca368b3b1c45022d8041107212c5792164a7c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-02 19:08:00 -0700

    Add Binary Strings

---
+++ Leetcode/src/ArraysAndStrings/AddBinary.java
@@ -0,0 +1,26 @@
+package ArraysAndStrings;
+
+public class AddBinary {
+
+    public String addBinary(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        int i = str1.length() - 1, j = str2.length() - 1, carry = 0;
+
+        while( i >= 0 || j >= 0){
+            int sum = carry;
+            if(j>=0) sum += str2.charAt(j--) - '0';
+            if(i>=0) sum += str1.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if(carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinary addBinary = new AddBinary();
+        String str1 = "1010";
+        String str2 = "1011";
+        System.out.println(addBinary.addBinary(str1, str2));
+    }
+}
