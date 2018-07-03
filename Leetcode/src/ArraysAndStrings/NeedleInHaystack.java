ffcdfe7ccb6f9bef4f865c449eae195736879573
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-02 19:47:56 -0700

    Needle in a Haystack String problem

---
+++ Leetcode/src/ArraysAndStrings/NeedleInHaystack.java
@@ -0,0 +1,40 @@
+package ArraysAndStrings;
+
+public class NeedleInHaystack {
+
+    public int needleInHaystack(String haystack, String needle) {
+        if(needle.length() == 0) {
+            return 0;
+        }
+
+        if(haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        if(haystack.length() == needle.length()) {
+            return haystack.compareTo(needle);
+        }
+
+        for (int i=0; i<haystack.length(); i++) {
+            int hIndex = i;
+            int nIndex = 0;
+
+            while(hIndex < haystack.length() && nIndex < needle.length() && haystack.charAt(hIndex) == needle.charAt(nIndex)) {
+                hIndex++;
+                nIndex++;
+            }
+
+            if(nIndex == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        NeedleInHaystack needleInHaystack = new NeedleInHaystack();
+        String haystack = "Hello";
+        String needle = "ll";
+        System.out.println(needleInHaystack.needleInHaystack(haystack, needle));
+    }
+}
