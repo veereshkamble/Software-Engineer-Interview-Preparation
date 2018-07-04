bc319da4568c5a4b2ee15a9733a7654f856a1e07
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-03 19:52:04 -0700

    Max Consecutive Ones

---
+++ Leetcode/src/ArraysAndStrings/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package ArraysAndStrings;
+
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, result = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0)
+                count = 0;
+            else {
+                count++;
+                result = Math.max(count, result);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,1,0,1,1,1,1,0};
+        System.out.println(MaxConsecutiveOnes.findMaxConsecutiveOnes(nums));
+    }
+}
