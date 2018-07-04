9002cfde79633b79faf3e990a1a8e733154141f7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-04 13:37:56 -0700

    Minimium Size Sub array solution understoof

--- Leetcode/src/ArraysAndStrings/MinimumSizeSubarraySum.java
+++ Leetcode/src/ArraysAndStrings/MinimumSizeSubarraySum.java
@@ -3,20 +3,28 @@ package ArraysAndStrings;
 public class MinimumSizeSubarraySum {
 
     public int minSubArrayLen(int s, int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
+        int i=0, j=0, sum=0, min = Integer.MAX_VALUE;
 
-        int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
+        while(j < nums.length) {
+            while(sum < s && j < nums.length) {
+                sum += nums[j];
+                j++;
+            }
 
-        while (j < nums.length) {
-            sum += nums[j++];
-
-            while (sum >= s) {
-                min = Math.min(min, j - i);
-                sum -= nums[i++];
+            if(sum >= s) {
+                while(sum >= s && i < j) {
+                    sum -= nums[i];
+                    i++;
+                }
+                min = Math.min(min, j-i+1);
             }
         }
-
         return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2, 3, 1, 2, 4, 3};
+        MinimumSizeSubarraySum minimumSizeSubarraySum = new MinimumSizeSubarraySum();
+        System.out.println(minimumSizeSubarraySum.minSubArrayLen(7, nums));
     }
 }
