9513bef89cf1e55804a53ae7f7536242e4454ac9
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-04 21:45:03 -0700

    Pascals Index by row

---
+++ Leetcode/src/ArraysAndStrings/PascalsIndex.java
@@ -0,0 +1,29 @@
+package ArraysAndStrings;
+
+import java.util.*;
+
+public class PascalsIndex {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pascalsIndex = new LinkedList<Integer>();
+        pascalsIndex.add(1);
+        if(rowIndex == 0) {
+           return pascalsIndex;
+        }
+        int multiplier = rowIndex, divider = 1;
+        long current = 1;
+
+        for(int i = 1; i < rowIndex + 1; i++) {
+            current = current * multiplier;
+            current = current / divider;
+            pascalsIndex.add((int)current);
+            multiplier--;
+            divider++;
+        }
+        return pascalsIndex;
+    }
+
+    public static void main(String[] args) {
+        PascalsIndex pascalsIndex = new PascalsIndex();
+        System.out.println(pascalsIndex.getRow(6));
+    }
+}
