52313e744ce7da5b4610459b404782dbaa57af21
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-04 20:37:02 -0700

    Rotate Array solved by me but using extra space- a new array

---
+++ Leetcode/src/ArraysAndStrings/RotateArray.java
@@ -0,0 +1,30 @@
+package ArraysAndStrings;
+
+import javafx.scene.transform.Rotate;
+
+public class RotateArray {
+    public int[] rotate(int[] nums, int k) {
+        int[] newNums = new int[nums.length];
+        int diff = nums.length - k;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= nums.length-k) {
+                newNums[i-diff] = nums[i];
+            } else {
+                newNums[i+k] = nums[i];
+            }
+        }
+        return newNums;
+    }
+
+    public static void main(String[] args) {
+        RotateArray rotateArray = new RotateArray();
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int k = 2;
+
+        int[] newNums = rotateArray.rotate(nums, k);
+        for(int i = 0; i < newNums.length; i++) {
+            System.out.print(newNums[i] + " ");
+        }
+    }
+}
