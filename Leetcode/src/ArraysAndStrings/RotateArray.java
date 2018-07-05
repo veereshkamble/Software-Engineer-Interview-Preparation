807a3f0bac738a789a7e9f2e503b33c36659c4c5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-04 20:44:06 -0700

    Rotate Array modified

--- Leetcode/src/ArraysAndStrings/RotateArray.java
+++ Leetcode/src/ArraysAndStrings/RotateArray.java
@@ -4,7 +4,12 @@ import javafx.scene.transform.Rotate;
 
 public class RotateArray {
     public int[] rotate(int[] nums, int k) {
+
         int[] newNums = new int[nums.length];
+        if(nums == null || nums.length == 0) return newNums;
+
+        if(k > nums.length) return newNums;
+
         int diff = nums.length - k;
 
         for (int i = 0; i < nums.length; i++) {
@@ -20,7 +25,7 @@ public class RotateArray {
     public static void main(String[] args) {
         RotateArray rotateArray = new RotateArray();
         int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
-        int k = 2;
+        int k = 8;
 
         int[] newNums = rotateArray.rotate(nums, k);
         for(int i = 0; i < newNums.length; i++) {
