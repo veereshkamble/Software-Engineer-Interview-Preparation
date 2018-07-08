c1ea80c9936ae3f2dc32d5f9ece13e8d83b13a81
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-08 00:31:37 -0700

    Linked List Intersection getIntersection method

--- Leetcode/src/LinkedList/LinkedListIntersection.java
+++ Leetcode/src/LinkedList/LinkedListIntersection.java
@@ -2,6 +2,31 @@ package LinkedList;
 
 public class LinkedListIntersection {
 
+    public Node getIntersectionNode(Node headA, Node headB) {
+        int lengthA = length(headA);
+        int lengthB = length(headB);
+
+        Node nodeA = headA;
+        Node nodeB = headB;
+
+        while(lengthA > lengthB) {
+            nodeA = nodeA.next;
+            lengthA--;
+        }
+
+        while(lengthA < lengthB) {
+            nodeB = nodeB.next;
+            lengthB--;
+        }
+
+        while(nodeA != nodeB) {
+            nodeA = nodeA.next;
+            nodeB = nodeB.next;
+        }
+
+        return nodeA;
+    }
+
     public int length(Node node) {
         int length = 0;
         while(node != null) {
