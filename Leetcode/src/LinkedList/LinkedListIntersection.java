dd93149b3b12e7d20358b65e2ed6c470e2e46c8a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-07 23:56:43 -0700

    Linked List Intersection get tail and size method

--- Leetcode/src/LinkedList/LinkedListIntersection.java
+++ Leetcode/src/LinkedList/LinkedListIntersection.java
@@ -10,5 +10,15 @@ public class LinkedListIntersection {
         }
     }
 
-    
+    public static Result getTailAndSize(Node list) {
+        if (list == null) return null;
+
+        int size = 1;
+        Node current = list;
+        while (current.next != null) {
+            size++;
+            current = current.next;
+        }
+        return new Result(current, size);
+    }
 }
