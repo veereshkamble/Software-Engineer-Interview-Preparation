2e5c92edfd30d631780fdb9438bd6dc226097189
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-07 23:55:37 -0700

    Linked List Intersection Result class

---
+++ Leetcode/src/LinkedList/LinkedListIntersection.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class LinkedListIntersection {
+    public static class Result {
+        public Node tail;
+        public int size;
+        public Result(Node tail, int size) {
+            this.tail = tail;
+            this.size = size;
+        }
+    }
+
+    
+}
