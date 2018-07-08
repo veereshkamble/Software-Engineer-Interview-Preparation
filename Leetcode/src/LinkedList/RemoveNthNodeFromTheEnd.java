e457025296beae11a9110d34d93c5f357a55a735
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-08 01:16:38 -0700

    Remove Nth node in one pass

--- Leetcode/src/LinkedList/RemoveNthNodeFromTheEnd.java
+++ Leetcode/src/LinkedList/RemoveNthNodeFromTheEnd.java
@@ -34,4 +34,24 @@ public class RemoveNthNodeFromTheEnd {
         }
         return length;
     }
+
+    public Node removeNthFromEnd(Node head, int n) {
+
+        Node start = new Node(0);
+        Node slow = start, fast = start;
+        slow.next = head;
+
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
 }
