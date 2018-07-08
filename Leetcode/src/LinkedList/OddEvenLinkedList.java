0cd890c3e85d544a500bc88caa6729655c9bbcfc
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-08 16:53:52 -0700

    Odd Even Linked List

---
+++ Leetcode/src/LinkedList/OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class OddEvenLinkedList {
+    public Node oddEvenList(Node head) {
+        if (head != null) {
+
+            Node odd = head, even = head.next, evenHead = even;
+
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
--- Leetcode/src/LinkedList/RemoveLinkedListElements.java
+++ Leetcode/src/LinkedList/RemoveLinkedListElements.java
@@ -36,4 +36,4 @@ public class RemoveLinkedListElements {
         return head;
     }
 }
-}
+
