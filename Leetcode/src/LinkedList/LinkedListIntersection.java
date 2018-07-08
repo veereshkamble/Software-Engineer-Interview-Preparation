13d2ae604fbbbd88308c682a34a1874ddbef9318
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-07 23:59:27 -0700

    Linked List Intersection method

--- Leetcode/src/LinkedList/LinkedListIntersection.java
+++ Leetcode/src/LinkedList/LinkedListIntersection.java
@@ -30,4 +30,32 @@ public class LinkedListIntersection {
         }
         return current;
     }
+    public static Node findIntersection(Node list1, Node list2) {
+        if (list1 == null || list2 == null) return null;
+
+        /* Get tail and sizes. */
+        Result result1 = getTailAndSize(list1);
+        Result result2 = getTailAndSize(list2);
+
+        /* If different tail nodes, then there's no intersection. */
+        if (result1.tail != result2.tail) {
+            return null;
+        }
+
+        /* Set pointers to the start of each linked list. */
+        Node shorter = result1.size < result2.size ? list1 : list2;
+        Node longer = result1.size < result2.size ? list2 : list1;
+
+        /* Advance the pointer for the longer linked list by the difference in lengths. */
+        longer = getKthNode(longer, Math.abs(result1.size - result2.size));
+
+        /* Move both pointers until you have a collision. */
+        while (shorter != longer) {
+            shorter = shorter.next;
+            longer = longer.next;
+        }
+
+        /* Return either one. */
+        return longer;
+    }
 }
