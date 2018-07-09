abb9db180e9b43489edabbad90b5c535d99b2557
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-09 13:20:24 -0700

    Flat Node flatten function

--- Leetcode/src/LinkedList/FlattenMultiLevelDoublyLinkedList.java
+++ Leetcode/src/LinkedList/FlattenMultiLevelDoublyLinkedList.java
@@ -2,10 +2,19 @@ package LinkedList;
 
 public class FlattenMultiLevelDoublyLinkedList {
 
-    public Node flatten(Node head) {
-        Node cur = head;
+    public FlatNode flatten(FlatNode head) {
+        FlatNode cur = head;
         while(cur != null) {
             if(cur.child != null) {
+                join(cur.child, cur, cur.next);
+                cur.child = null;
+            }
+            cur = cur.next;
         }
+        return head;
+    }
+
+    public void join(FlatNode node, FlatNode before, FlatNode after) {
+        
     }
 }
