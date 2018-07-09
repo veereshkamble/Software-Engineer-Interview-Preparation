dc860ef7fbfb2bebbb3851507402dbf0a0aafab5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-09 00:39:11 -0700

    Doubly Linked List

--- Leetcode/src/LinkedList/DoublyLinkedList.java
+++ Leetcode/src/LinkedList/DoublyLinkedList.java
@@ -1,4 +1,23 @@
 package LinkedList;
 
 public class DoublyLinkedList {
+    DoublyListNode head;
+
+    public DoublyLinkedList() {
+        head = null;
+    }
+
+    private DoublyListNode getTail() {
+        DoublyListNode curr = head;
+        while(curr != null && curr.next != null) {
+            curr = curr.next;
+        }
+        return curr;
+    }
+
+    public int get(int index) {
+        DoublyListNode curr = getNode(index);
+        return curr == null ? -1 : curr.val;
+    }
+    
 }
