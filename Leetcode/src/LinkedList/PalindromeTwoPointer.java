6a5a56e4508f17528dc1d6785f94652767acfcc7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-08 21:27:00 -0700

    Palindrome Linked List using Two pointer technique

---
+++ Leetcode/src/LinkedList/PalindromeTwoPointer.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+public class PalindromeTwoPointer {
+
+    public boolean isPalindrome(Node head) {
+        Node fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public Node reverse(Node head) {
+        Node prev = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
