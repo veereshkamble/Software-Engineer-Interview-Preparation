a101fc592893e932f25683a868d5e66814ad978f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-11 19:51:17 -0700

    Hash Map implementation without using bukt in libraries

---
+++ Leetcode/src/HashTables/MyHashMap.java
@@ -0,0 +1,24 @@
+package HashTables;
+
+public class MyHashMap {
+
+    /* Initialize your data Structure here */
+    public MyHashMap() {
+
+    }
+
+    /*value will always be positive */
+    public void put(int key, int value) {
+        
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int jey) {
+
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int jey) {
+
+    }
+}
