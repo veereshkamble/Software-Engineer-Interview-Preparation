edc87078f31567e34c22a6e1753f7b01945ed655
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-11 19:26:02 -0700

    Starting Hash Tables, pseudocode to design a hash set without using built in libraries

---
+++ Leetcode/src/HashTables/MyHashSet.java
@@ -0,0 +1,25 @@
+package HashTables;
+
+public class MyHashSet {
+
+    private int buckets = 1000;
+    private int itemsPerBucket = 1001;
+    private boolean[][] table;
+
+    /* Initialize your data structure here*/
+    public MyHashSet() {
+
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public boolean contains (int key) {
+        
+    }
+}
