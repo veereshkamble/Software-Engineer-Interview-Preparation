f187da90c89e972b1560b59ca173ae4a45a87ab3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-11 19:31:16 -0700

    HashSet add() function

--- Leetcode/src/HashTables/MyHashSet.java
+++ Leetcode/src/HashTables/MyHashSet.java
@@ -8,18 +8,32 @@ public class MyHashSet {
 
     /* Initialize your data structure here*/
     public MyHashSet() {
+        table = new boolean[buckets][];
+    }
 
+    public int hash(int key) {
+        return key % buckets;
+    }
+
+    public int pos(int key) {
+        return key / buckets;
     }
 
     public void add(int key) {
+        int hashKey = hash(key);
+
+        if(table[hashKey] == null) {
+            table[hashKey] = new boolean[itemsPerBucket];
+        }
+        table[hashKey][pos(key)] = true;
 
     }
 
     public void remove(int key) {
-
+        
     }
 
     public boolean contains (int key) {
-        
+
     }
 }
