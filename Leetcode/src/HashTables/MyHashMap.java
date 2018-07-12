6667b5cf6d778364263fbf0cce4fcbe1e2535ac3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-11 23:35:05 -0700

    Hash Map put method

--- Leetcode/src/HashTables/MyHashMap.java
+++ Leetcode/src/HashTables/MyHashMap.java
@@ -1,5 +1,7 @@
 package HashTables;
 
+import java.util.*;
+
 public class MyHashMap {
     private int buckets = 1000;
     private int itemsPerBucket = 1001;
@@ -7,16 +9,29 @@ public class MyHashMap {
 
     /* Initialize your data Structure here */
     public MyHashMap() {
-        table = new int[buckets]][];
+        table = new int[buckets][];
+    }
+    public int hash(int key) {
+        return key % buckets;
+    }
+
+    public int pos(int key) {
+        return key / buckets;
     }
 
     /*value will always be positive */
     public void put(int key, int value) {
+        int hashKey = hash(key);
 
+        if(table[hashKey] == null) {
+            table[hashKey] = new int[itemsPerBucket];
+            Arrays.fill(table[hashKey], -1);
+        }
+        table[hashKey][pos(key)] = value;
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int jey) {
+    public int get(int key) {
 
     }
 
