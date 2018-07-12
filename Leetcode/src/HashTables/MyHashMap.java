cf2c29bc90ceac0fe12ae1ddb68e6d291372db60
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-11 19:54:21 -0700

    Hash Map implelementation

--- Leetcode/src/HashTables/MyHashMap.java
+++ Leetcode/src/HashTables/MyHashMap.java
@@ -1,15 +1,18 @@
 package HashTables;
 
 public class MyHashMap {
+    private int buckets = 1000;
+    private int itemsPerBucket = 1001;
+    private int[][] table;
 
     /* Initialize your data Structure here */
     public MyHashMap() {
-
+        table = new int[buckets]][];
     }
 
     /*value will always be positive */
     public void put(int key, int value) {
-        
+
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
