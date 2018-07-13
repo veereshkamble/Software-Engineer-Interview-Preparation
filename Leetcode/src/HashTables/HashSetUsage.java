7d97365f6ddd97a4ed1136d9aed2157df696b336
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-13 13:45:17 -0700

    Using Hash Set Initialization

---
+++ Leetcode/src/HashTables/HashSetUsage.java
@@ -0,0 +1,11 @@
+package HashTables;
+import java.util.HashSet;
+import java.util.Set
+
+public class HashSetUsage {
+    public static void main(String[] args) {
+        // 1. Initialize the hash set
+        Set<Integer> hashSet = new HashSet<>();
+
+    }
+}
