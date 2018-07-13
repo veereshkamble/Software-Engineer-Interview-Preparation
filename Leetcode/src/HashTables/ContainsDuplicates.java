938f2e67c5d10b3c746d6bfd4ca0703c95f3cc42
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-13 16:34:30 -0700

    using hash set to find if an array contains duplicates

---
+++ Leetcode/src/HashTables/ContainsDuplicates.java
@@ -0,0 +1,18 @@
+package HashTables;
+
+import java.util.*;
+
+public class ContainsDuplicates {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<>();
+
+        for(int num : nums) {
+            if(hashSet.contains(num)) {
+                return true;
+            }
+            hashSet.add(num);
+        }
+        return false;
+    }
+}
