f2eedc9f4c1e27a6a570fbecd802d62643b3b46c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 14:51:32 -0700

    Intersection

---
+++ Leetcode/src/HashTables/Intersection.java
@@ -0,0 +1,13 @@
+package HashTables;
+import java.util.*;
+
+public class Intersection {
+
+    public int[] instersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        //Add all elements from array 1 to the hash set
+    }
+}
