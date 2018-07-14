3906bdf87f3bb43b3c9a22dbdff26d97b2d99e35
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 14:58:34 -0700

    Intersecting arrays method() completed

--- Leetcode/src/HashTables/Intersection.java
+++ Leetcode/src/HashTables/Intersection.java
@@ -9,5 +9,21 @@ public class Intersection {
         ArrayList<Integer> result = new ArrayList<Integer>();
 
         //Add all elements from array 1 to the hash set
+        for(int i = 0; i< nums1.length; i++) {
+            hashSet.add(nums1[i]);
+        }
+
+        //if the number is present in the hashset then add the number ot the result array and remove it from the hashset
+        for(int j = 0; j < nums2.length; j++) {
+            if(hashSet.contains(nums2[j])) {
+                result.add(nums2[j]);
+                hashSet.remove(nums2[j]);
+            }
+        }
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArray[i] = result.get(i);
+        }
+        return resultArray;
     }
 }
