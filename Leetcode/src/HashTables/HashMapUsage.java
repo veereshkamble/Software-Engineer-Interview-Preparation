214e6009b41105fa6bb6ac82bc73cb787cbf7fb7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 18:47:04 -0700

    More hash map methods

--- Leetcode/src/HashTables/HashMapUsage.java
+++ Leetcode/src/HashTables/HashMapUsage.java
@@ -10,6 +10,20 @@ public class HashMapUsage {
         hashMap.putIfAbsent(0,0);
         hashMap.putIfAbsent(2,2);
 
+        // 3. insert a new (key, value) pair or update the value of existing key
+        hashMap.put(1,3);
+        hashMap.put(1,1);
+
+        //4. get the value of a specific key
+        System.out.println("The value of key 1 is : " +hashMap.get(1));
+
+        //5. delete a key
+        hashMap.remove(2);
+
+        // 6. check if a key is in the hash map
+        if(hashMap.containsKey(2)){
+            System.out.println("Key 2 is not in the hash map");
+        }
         
     }
 }
