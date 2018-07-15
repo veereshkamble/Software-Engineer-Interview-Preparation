f0c3ab5cd6f9d29be835e5a60786c1486ebe8a01
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 19:45:12 -0700

    Two Sum using Hash Map

---
+++ Leetcode/src/HashTables/TwoSum.java
@@ -0,0 +1,20 @@
+package HashTables;
+import java.util.*;
+
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target - nums[i])) {
+                return new int[] {hashMap.get(target - nums[i]), i};
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+
+    
+}
