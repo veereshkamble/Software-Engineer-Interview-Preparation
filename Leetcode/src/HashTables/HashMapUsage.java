29ef73552310c66218f510ae9e28d2dc6796ba5e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 18:42:35 -0700

    Hash Map Usage methods()

---
+++ Leetcode/src/HashTables/HashMapUsage.java
@@ -0,0 +1,15 @@
+package HashTables;
+import java.util.*;
+
+public class HashMapUsage {
+    public static void main(String[] args) {
+        // 1. Initialize a Hash Map
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        // 2. insert a new (key, value) pair
+        hashMap.putIfAbsent(0,0);
+        hashMap.putIfAbsent(2,2);
+
+        
+    }
+}
