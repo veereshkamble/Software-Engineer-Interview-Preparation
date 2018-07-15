75161915e29df71c2ae0a0f6a6db37945c5e7768
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 19:49:50 -0700

    Two Sum Driver method

--- Leetcode/src/HashTables/TwoSum.java
+++ Leetcode/src/HashTables/TwoSum.java
@@ -16,5 +16,14 @@ public class TwoSum {
         return new int[]{};
     }
 
-    
+    public static void main(String[] args) {
+        int[] nums = new int[] {2, 7, 11, 15};
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(nums, 17);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+    }
 }
