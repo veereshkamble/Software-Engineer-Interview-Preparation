8f12ff5aa35949e0dd414c716ff12f800edaa739
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-14 20:42:54 -0700

    Isomorphic Strings bug fixes. Working code

--- Leetcode/src/HashTables/IsomorphicStrings.java
+++ Leetcode/src/HashTables/IsomorphicStrings.java
@@ -6,18 +6,32 @@ public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
 
         char[] string1 = s.toCharArray();
-        char[] string2 = s.toCharArray();
+        char[] string2 = t.toCharArray();
         HashMap<Character, Character> hashMap = new HashMap<Character, Character>();
         int i = 0;
         while(i < string1.length) {
-            hashMap.put(string1[i], string2[i]);
+            if(!hashMap.containsKey(string1[i])) {
+                hashMap.put(string1[i], string2[i]);
+            }
             i++;
         }
-        while(i < string1.length) {
-            if(hashMap.get(string1[i]) != string2[i]) {
+        int j = 0;
+        while(j < string1.length) {
+            System.out.println(hashMap.get(string1[j]));
+            if(hashMap.get(string1[j]) != string2[j]) {
                 return false;
             }
+            j++;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        IsomorphicStrings isomorphicStrings = new IsomorphicStrings();
+        String string1 = "paper";
+        String string2 = "title";
+
+        System.out.println(isomorphicStrings.isIsomorphic(string1, string2));
+
     }
 }
