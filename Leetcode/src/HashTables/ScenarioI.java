da318283c3661ad1a07a6ad8b1028c8a8d16757f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-15 15:56:43 -0700

    Usage Scanrio I Hash Map

---
+++ Leetcode/src/HashTables/ScenarioI.java
@@ -0,0 +1,23 @@
+package HashTables;
+
+public class ScenarioI {
+
+    /*
+     * Template for using hash map to find duplicates.
+     * Replace ReturnType with the actual type of your return value.
+     */
+    ReturnType aggregateByKey_hashmap(List<Type>& keys) {
+        // Replace Type and InfoType with actual type of your key and value
+        Map<Type, InfoType> hashmap = new HashMap<>();
+        for (Type key : keys) {
+            if (hashmap.containsKey(key)) {
+                if (hashmap.get(key) satisfies the requirement) {
+                    return needed_information;
+                }
+            }
+            // Value can be any information you needed (e.g. index)
+            hashmap.put(key, value);
+        }
+        return needed_information;
+    }
+}
