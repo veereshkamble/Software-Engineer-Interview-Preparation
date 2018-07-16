5cace2fd2ec3dad2dfbf649a4a71397f825050bf
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-16 12:57:23 -0700

    Designing a Logger class using Hash Map. QUestion asked in Google phone interview to Parvez. I got to get this right

---
+++ Leetcode/src/HashTables/Logger.java
@@ -0,0 +1,17 @@
+package HashTables;
+
+public class Logger {
+
+    /* Initialize your data structure here */
+    public Logger() {
+
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     If this method returns false, the message will not be printed.
+     The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+
+        return true;
+    }
+}
