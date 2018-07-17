532fd44e60e8c363ab88c59cd54aacef2c3134b4
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-16 17:20:51 -0700

    postorder method

--- Leetcode/src/HashTables/FindDuplicateSubtrees.java
+++ Leetcode/src/HashTables/FindDuplicateSubtrees.java
@@ -14,4 +14,20 @@ public class FindDuplicateSubtrees {
         postorder(root, new HashMap<>(), res);
         return res;
     }
+
+    public String postorder(TreeNode cur, Map<String, Integer> map, List<TreeNode> res) {
+        if (cur == null) return "#";
+        StringBuilder sb = new StringBuilder();
+        sb.append(cur.val).append(",");
+        sb.append(postorder(cur.left, map, res)).append(",");
+        sb.append(postorder(cur.right, map, res));
+
+        String serial = sb.toString();
+
+        if (map.getOrDefault(serial, 0) == 1)
+            res.add(cur);
+
+        map.put(serial, map.getOrDefault(serial, 0) + 1);
+        return serial;
+    }
 }
