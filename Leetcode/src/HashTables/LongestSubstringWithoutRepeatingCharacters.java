e095f85da75aaa462e08afad1e1479a842690a1b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-17 17:08:09 -0700

    Longest Substring without repeating characters main driver function

--- Leetcode/src/HashTables/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode/src/HashTables/LongestSubstringWithoutRepeatingCharacters.java
@@ -16,4 +16,10 @@ public class LongestSubstringWithoutRepeatingCharacters {
         }
         return max;
     }
+
+    public static void main(String[] args) {
+        String str = "abcabcbb";
+        LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(longestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring(str));
+    }
 }
