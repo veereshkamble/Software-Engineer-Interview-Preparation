491799a8f0816abbbb0b7c03537e88a2d7ad7437
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-17 17:32:20 -0700

    Two Sum Data Structure Design - methods

--- Leetcode/src/HashTables/TwoSumIII.java
+++ Leetcode/src/HashTables/TwoSumIII.java
@@ -1,5 +1,13 @@
 package HashTables;
+import java.util.*;
 
 public class TwoSumIII {
-    
+    Set<Integer> sum;
+    Set<Integer> num;
+
+    /** Initialize your data structure here. */
+    public TwoSumIII() {
+        sum = new HashSet<Integer>();
+        num = new HashSet<Integer>();
+    }
 }
