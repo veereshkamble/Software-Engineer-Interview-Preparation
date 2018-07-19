6753dffc9b731956f9fc41370b1abcd85cf688d2
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 23:46:24 -0700

    Postorder Traversal main method

--- Leetcode/src/Trees/PostorderIterative.java
+++ Leetcode/src/Trees/PostorderIterative.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 public class PostorderIterative {
 
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
         if(root == null) {
             return result;
@@ -27,5 +27,42 @@ public class PostorderIterative {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node5.left = node2;
+        node5.right = node6;
+
+        node2.left = node1;
+        node2.right = node4;
+
+        node1.left = null;
+        node1.right = null;
+
+        node4.left = node3;
+        node4.right = null;
+
+        node6.left = null;
+        node6.right = node7;
+
+        node7.left = null;
+        node7.right = null;
+
+        PostorderIterative postorder = new PostorderIterative();
+        List<Integer> result = new ArrayList<Integer>();
+
+        result = postorder.postorderTraversal(node5);
+
+        for(int node : result) {
+            System.out.print(node + " ");
+        }
     }
 }
