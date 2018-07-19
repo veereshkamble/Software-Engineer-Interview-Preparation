2f301393f32edc3f05865258967d39283dc16b8c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 23:41:43 -0700

    PostOrder main method

--- Leetcode/src/Trees/PostorderRecursive.java
+++ Leetcode/src/Trees/PostorderRecursive.java
@@ -13,4 +13,41 @@ public class PostorderRecursive {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node5.left = node2;
+        node5.right = node6;
+
+        node2.left = node1;
+        node2.right = node4;
+
+        node1.left = null;
+        node1.right = null;
+
+        node4.left = node3;
+        node4.right = null;
+
+        node6.left = null;
+        node6.right = node7;
+
+        node7.left = null;
+        node7.right = null;
+
+        PostorderRecursive postorder = new PostorderRecursive();
+        List<Integer> result = new ArrayList<Integer>();
+
+        result = postorder.postorderTraversal(node5);
+
+        for(int node : result) {
+            System.out.print(node + " ");
+        }
+    }
 }
