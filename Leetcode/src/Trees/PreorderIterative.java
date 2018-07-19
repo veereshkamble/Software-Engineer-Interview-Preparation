0b58593f24302e16f890aaf926a960c474104f26
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 18:52:30 -0700

    Pre Order Traversal Iterative solution

---
+++ Leetcode/src/Trees/PreorderIterative.java
@@ -0,0 +1,27 @@
+package Trees;
+import java.util.*;
+
+public class PreorderIterative {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
