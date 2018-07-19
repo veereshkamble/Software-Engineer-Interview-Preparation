945859966499d396ec73212f5f3f9b7d66e96af1
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 23:59:15 -0700

    Post order traversal recursive solution from Leet Code

---
+++ Leetcode/src/Trees/PostorderRecursiveLeetCode.java
@@ -0,0 +1,18 @@
+package Trees;
+import java.util.*;
+
+public class PostorderRecursiveLeetCode {
+    private void postorderTraversal(TreeNode root, List<Integer> answer) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left, answer);   // traverse left subtree
+        postorderTraversal(root.right, answer);  // traverse right subtree
+        answer.add(root.val);                    // visit the root
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        postorderTraversal(root, answer);
+        return answer;
+    }
+}
