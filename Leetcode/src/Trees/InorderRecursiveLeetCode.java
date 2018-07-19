cfa6e0e9a9573e3366eb3fd435779609371bea1b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 23:57:36 -0700

    In order Traversal recursive solution from LeetCode

---
+++ Leetcode/src/Trees/InorderRecursiveLeetCode.java
@@ -0,0 +1,18 @@
+package Trees;
+import java.util.*;
+
+public class InorderRecursiveLeetCode {
+    private void inorderTraversal(TreeNode root, List<Integer> answer) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, answer);   // traverse left subtree
+        answer.add(root.val);                  // visit the root
+        inorderTraversal(root.right, answer);  // traverse right subtree
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        inorderTraversal(root, answer);
+        return answer;
+    }
+}
