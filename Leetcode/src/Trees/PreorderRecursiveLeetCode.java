0fc1baecafaabe92ac2921bdb1dc6c4e4e4ce8a5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-18 23:56:06 -0700

    Preorder traversal iterative solution from Leetcode

---
+++ Leetcode/src/Trees/PreorderRecursiveLeetCode.java
@@ -0,0 +1,19 @@
+package Trees;
+import java.util.*;
+
+public class PreorderRecursiveLeetCode {
+
+    private void preorderTraversal(TreeNode root, List<Integer> answer) {
+        if (root == null) {
+            return;
+        }
+        answer.add(root.val);                   // visit the root
+        preorderTraversal(root.left, answer);   // traverse left subtree
+        preorderTraversal(root.right, answer);  // traverse right subtree
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        preorderTraversal(root, answer);
+        return answer;
+    }
+}
