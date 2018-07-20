45907e4a1a3c7323d7d3a3fe6edbb9cac5104f2c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-20 16:23:12 -0700

    Path Sum Iterative main driver method

--- Leetcode/src/Trees/PathSumIterative.java
+++ Leetcode/src/Trees/PathSumIterative.java
@@ -26,4 +26,34 @@ public class PathSumIterative {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node11 = new TreeNode(11);
+        TreeNode node13 = new TreeNode(13);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+
+        node5.left = node4;
+        node5.right = node8;
+
+        node4.left = node11;
+
+        node11.left = node7;
+        node11.right = node2;
+
+        node8.left = node13;
+        node8.right = node9;
+
+        node9.right = node1;
+
+        PathSumIterative pathSumIterative = new PathSumIterative();
+        System.out.println(pathSumIterative.hasPathSum(node5, 22));
+
+
+    }
 }
