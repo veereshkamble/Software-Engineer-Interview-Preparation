e0a26534e83f0476aae02367020aa3d7700db6f1
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-19 19:36:47 -0700

    Symmetric tree Recursive main driver program

--- Leetcode/src/Trees/SymmetricTreeRecursive.java
+++ Leetcode/src/Trees/SymmetricTreeRecursive.java
@@ -15,4 +15,27 @@ public class SymmetricTreeRecursive {
         }
         return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
+
+    public static void main(String[] args) {
+        TreeNode nodeA = new TreeNode(1);
+        TreeNode nodeB = new TreeNode(2);
+        TreeNode nodeC = new TreeNode(2);
+        TreeNode nodeD = new TreeNode(3);
+        TreeNode nodeE = new TreeNode(4);
+        TreeNode nodeF = new TreeNode(4);
+        TreeNode nodeG = new TreeNode(3);
+
+        nodeA.left = nodeB;
+        nodeA.right = nodeC;
+
+        nodeB.left = nodeD;
+        nodeB.right = nodeE;
+
+        nodeC.left = nodeF;
+        nodeC.right = nodeG;
+
+        SymmetricTreeRecursive symmetricTreeRecursive = new SymmetricTreeRecursive();
+        System.out.println(symmetricTreeRecursive.isSymmetric(nodeA));
+
+    }
 }
