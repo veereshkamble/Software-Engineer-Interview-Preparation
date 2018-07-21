f1420bbc6f909cf11f9360e6d11b1ca0b37d2920
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-21 15:26:03 -0700

    Construct Binary Tree From Oreorder and Inorder Iterative main method

--- Leetcode/src/Trees/BinaryTreeFromInPreIterative.java
+++ Leetcode/src/Trees/BinaryTreeFromInPreIterative.java
@@ -48,4 +48,12 @@ public class BinaryTreeFromInPreIterative {
 
         return root;
     }
+
+    public static void main(String[] args) {
+        int[] inorder = new int[] {9, 3, 15, 20, 7};
+        int[] postorder = new int[] {9, 15, 7, 20, 3};
+
+        BinaryTreeFromInPreIterative binaryTreeFromInPreIterative = new BinaryTreeFromInPreIterative();
+        System.out.println(binaryTreeFromInPreIterative.buildTree(inorder, postorder));
+    }
 }
