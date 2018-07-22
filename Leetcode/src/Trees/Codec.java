d1401434464cdd0a4513627f1eefc9c869d14af5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-22 16:52:25 -0700

    Codec Serialize method:

---
+++ Leetcode/src/Trees/Codec.java
@@ -0,0 +1,24 @@
+package Trees;
+
+public class Codec {
+
+    private static final String spliter = ",";
+    private static final String NN = "X";
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+
+    private void buildString(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append(NN).append(spliter);
+        } else {
+            sb.append(node.val).append(spliter);
+            buildString(node.left, sb);
+            buildString(node.right,sb);
+        }
+    }
+}
