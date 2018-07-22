fc12a293f9fff1b22852cb198d4acf4f3cfc092a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-22 15:31:29 -0700

    Populate Next Rught node

---
+++ Leetcode/src/Trees/PopulateNextRightPointerRecursive.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class PopulateNextRightPointerRecursive {
+
+    public void connect(TreeLinkNode root) {
+        if(root == null)
+            return;
+
+        if(root.left != null){
+            root.left.next = root.right;
+            if(root.next != null)
+                root.right.next = root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+    }
+}
