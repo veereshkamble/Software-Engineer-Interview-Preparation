7627a736e8eb9815f559acd50b505ebfe1305e36
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-22 16:35:50 -0700

    Lowest Common Ancestor main method

--- Leetcode/src/Trees/LowestCommonAncestor.java
+++ Leetcode/src/Trees/LowestCommonAncestor.java
@@ -9,4 +9,28 @@ public class LowestCommonAncestor {
         if(left != null && right != null)   return root;
         return left != null ? left : right;
     }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node1.left = node2;
+        node1.right = node3;
+
+        node2.left = node4;
+        node2.right = node5;
+
+
+        node3.right = node7;
+
+        LowestCommonAncestor lowestCommonAncestor = new LowestCommonAncestor();
+        System.out.println(lowestCommonAncestor.lowestCommonAncestor(node1, node4, node5));
+
+
+    }
 }
