cbeeea896820a9af40a748e85ada1609822a679e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-22 15:32:48 -0700

    Tree Link Node cladd

---
+++ Leetcode/src/Trees/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class TreeLinkNode {
+
+    int val;
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+     TreeLinkNode(int x) {
+         this.val = x;
+     }
+}
