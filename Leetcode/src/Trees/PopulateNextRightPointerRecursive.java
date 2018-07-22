ae3e5c2f855f959c474e51a846529f2215eb835a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-22 15:36:26 -0700

    Populate Next Rught node main method

--- Leetcode/src/Trees/PopulateNextRightPointerRecursive.java
+++ Leetcode/src/Trees/PopulateNextRightPointerRecursive.java
@@ -15,4 +15,28 @@ public class PopulateNextRightPointerRecursive {
         connect(root.left);
         connect(root.right);
     }
+
+    public static void main(String[] args) {
+        TreeLinkNode node1 = new TreeLinkNode(1);
+        TreeLinkNode node2 = new TreeLinkNode(2);
+        TreeLinkNode node3 = new TreeLinkNode(3);
+        TreeLinkNode node4 = new TreeLinkNode(4);
+        TreeLinkNode node5 = new TreeLinkNode(5);
+        TreeLinkNode node6 = new TreeLinkNode(6);
+        TreeLinkNode node7 = new TreeLinkNode(7);
+
+        node1.left = node2;
+        node1.right = node3;
+
+        node2.left = node4;
+        node2.right = node5;
+
+        node3.left = node6;
+        node3.right = node7;
+
+        PopulateNextRightPointerRecursive populateNextRightPointerRecursive = new PopulateNextRightPointerRecursive();
+        System.out.println(populateNextRightPointerRecursive.connect(node1));
+
+
+    }
 }
