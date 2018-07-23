644ff6b8b8685caaea848704a69a7c3150ea1072
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-23 15:03:21 -0700

    Validate Binary Search Tree Iterative

--- Leetcode/src/Trees/ValidateBinarySearchTreeIterative.java
+++ Leetcode/src/Trees/ValidateBinarySearchTreeIterative.java
@@ -19,4 +19,35 @@ public class ValidateBinarySearchTreeIterative {
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node5.left = node2;
+        node5.right = node6;
+
+        node2.left = node1;
+        node2.right = node4;
+
+        node1.left = null;
+        node1.right = null;
+
+        node4.left = node3;
+        node4.right = null;
+
+        node6.left = null;
+        node6.right = node7;
+
+        node7.left = null;
+        node7.right = null;
+
+        ValidateBinarySearchTreeIterative validateBinarySearchTreeIterative = new ValidateBinarySearchTreeIterative();
+        System.out.println(validateBinarySearchTreeIterative.isValidBST(node5));
+    }
 }
