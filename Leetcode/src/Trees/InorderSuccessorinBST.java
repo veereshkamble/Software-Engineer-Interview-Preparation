deb0747dbbb6f6bf799a35443bf6cec510828bf2
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-23 16:48:32 -0700

    Inorder successor in binary search tree

---
+++ Leetcode/src/Trees/InorderSuccessorinBST.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class InorderSuccessorinBST {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode ret = null;
+
+        while(root != null) {
+            if(root.val <= p.val) {
+                root = root.right;
+            } else {
+                ret = root;
+                root = root.left;
+            }
+        }
+        return ret;
+    }
+
+
+}
