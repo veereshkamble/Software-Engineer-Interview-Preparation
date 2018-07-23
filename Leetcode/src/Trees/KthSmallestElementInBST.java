26b4926958e916f7f40a1c823a993c5715b160f5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-23 14:47:06 -0700

    Kth Smallest Element in a Binary Search Tree

---
+++ Leetcode/src/Trees/KthSmallestElementInBST.java
@@ -0,0 +1,19 @@
+package Trees;
+import java.util.*;
+
+public class KthSmallestElementInBST {
+
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(--k == 0) break;
+            root = root.right;
+        }
+        return root.val;
+    }
+}
