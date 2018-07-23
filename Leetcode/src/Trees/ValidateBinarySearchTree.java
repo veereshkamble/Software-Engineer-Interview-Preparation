e03d098afb001ac81a7d2f7853d004da8eb94be3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-23 14:13:42 -0700

    Validate Binary Search Tree mainmethod

--- Leetcode/src/Trees/ValidateBinarySearchTree.java
+++ Leetcode/src/Trees/ValidateBinarySearchTree.java
@@ -11,4 +11,16 @@ public class ValidateBinarySearchTree {
         if (root.val >= maxVal || root.val <= minVal) return false;
         return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+
+        node2.left = node1;
+        node2.right = node3;
+
+        ValidateBinarySearchTree validateBinarySearchTree = new ValidateBinarySearchTree();
+        System.out.println(validateBinarySearchTree.isValidBST(node2));
+    }
 }
