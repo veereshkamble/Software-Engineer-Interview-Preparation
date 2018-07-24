6cdeed98164914259db29f207e44942ed424d684
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-23 17:05:40 -0700

    Inorder predecessor in binary search tree recursive main method

--- Leetcode/src/Trees/InorderPredecessorInBSTRecursive.java
+++ Leetcode/src/Trees/InorderPredecessorInBSTRecursive.java
@@ -2,16 +2,47 @@ package Trees;
 
 public class InorderPredecessorInBSTRecursive {
 
-    public TreeNode successor(TreeNode root, TreeNode p) {
+    public TreeNode predecessor(TreeNode root, TreeNode p) {
         if (root == null)
             return null;
 
         if (root.val >= p.val) {
-            return successor(root.left, p);
+            return predecessor(root.left, p);
         } else {
-            TreeNode right = successor(root.right, p);
+            TreeNode right = predecessor(root.right, p);
             return (right != null) ? right : root;
         }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node5.left = node2;
+        node5.right = node6;
+
+        node2.left = node1;
+        node2.right = node4;
+
+        node1.left = null;
+        node1.right = null;
+
+        node4.left = node3;
+        node4.right = null;
+
+        node6.left = null;
+        node6.right = node7;
+
+        node7.left = null;
+        node7.right = null;
+
+        InorderPredecessorInBSTRecursive inorderPredecessorInBSTRecursive = new InorderPredecessorInBSTRecursive();
+        System.out.println(inorderPredecessorInBSTRecursive.predecessor(node5, node2).val);
     }
 
 }
