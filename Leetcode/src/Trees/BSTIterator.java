39c20f1b717595ce254082daec40d001d3c964fa
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-24 16:21:28 -0700

    Binary Search Tree custom Iterator

---
+++ Leetcode/src/Trees/BSTIterator.java
@@ -0,0 +1,36 @@
+package Trees;
+import java.util.*;
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+
+    //Inititalize your data structure here
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    //@return whether we have a next smallest number or not
+    public boolean hasNext() {
+
+    }
+
+    //@return the next smallest number
+    public int next() {
+
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
