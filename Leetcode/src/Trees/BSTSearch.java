b368f5da542e1c5de151c89f7d729bb5a0acefa3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-24 17:24:17 -0700

    Search in a Binary Search tree search method()

---
+++ Leetcode/src/Trees/BSTSearch.java
@@ -0,0 +1,22 @@
+package Trees;
+
+public class BSTSearch {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode cur = root;
+
+        while(cur != null) {
+            if(cur.val == val) {
+                return cur;
+            } else if(cur.val < val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return null;
+    }
+}
