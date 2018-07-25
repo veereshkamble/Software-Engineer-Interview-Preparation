8cecb624862d0473fb0628b90ea49f0676ac906f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-24 17:41:00 -0700

    Search inBinary Search Tree Recursive LeetCode main method

--- Leetcode/src/Trees/BSTSearchRecursiveLeetCode.java
+++ Leetcode/src/Trees/BSTSearchRecursiveLeetCode.java
@@ -11,7 +11,7 @@ public class BSTSearchRecursiveLeetCode {
      *     TreeNode(int x) { val = x; }
      * }
      */
-    class Solution {
+
         public TreeNode searchBST(TreeNode root, int target) {
             if (root == null || root.val == target) {
                 return root;
@@ -21,5 +21,36 @@ public class BSTSearchRecursiveLeetCode {
             }
             return searchBST(root.right, target);
         }
+    
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node5.left = node2;
+        node5.right = node6;
+
+        node2.left = node1;
+        node2.right = node4;
+
+        node1.left = null;
+        node1.right = null;
+
+        node4.left = node3;
+        node4.right = null;
+
+        node6.left = null;
+        node6.right = node7;
+
+        node7.left = null;
+        node7.right = null;
+
+        BSTSearchRecursiveLeetCode bstSearch = new BSTSearchRecursiveLeetCode();
+        System.out.println(bstSearch.searchBST(node5, 7).val);
     }
 }
