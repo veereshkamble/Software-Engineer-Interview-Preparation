8ca5cce32b5ac476deb1cd71dade1162d6601be7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-25 12:50:58 -0700

    Kth Largest Element in a stream boiler plate code

---
+++ Leetcode/src/Trees/KthLargestElementInAStream.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class KthLargestElementInAStream {
+
+    public KthLargestElementInAStream (int k, int[] nums) {
+
+    }
+
+    public int add(int val) {
+        
+    }
+}
