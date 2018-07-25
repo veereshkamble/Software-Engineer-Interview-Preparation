fbc6fec04a2742630da439773f1abd8cd18c3230
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-25 13:37:03 -0700

    Lowest COmmon ANcestor Recursivve

---
+++ Leetcode/src/Trees/LowestCommonAncestorRecursive.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class LowestCommonAncestorRecursive {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if( root == null) {
+            return null;
+        }
+
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    
+}
