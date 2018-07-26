8f3658de053d221be3a256f13e592c65b239cef7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-25 23:28:50 -0700

    N-ary Node class

---
+++ Leetcode/src/Trees/NaryNode.java
@@ -0,0 +1,17 @@
+package Trees;
+import java.util.*;
+
+public class NaryNode {
+
+    public int val;
+    public List<NaryNode> children;
+
+    public NaryNode() {
+
+    }
+
+    public NaryNode(int val, List<NaryNode> children) {
+        this.val = val;
+        this.children = children;
+    }
+}
