ab0493d11734176ee5cedd2491e95a5333124f07
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 15:44:27 -0700

    Changes to Nary Node main method

--- Leetcode/src/Trees/NaryTreePreorderRecursive.java
+++ Leetcode/src/Trees/NaryTreePreorderRecursive.java
@@ -14,4 +14,31 @@ public class NaryTreePreorderRecursive {
 
         return list;
     }
+
+    public static void main(String[] args) {
+        NaryNode node1 = new NaryNode();
+        NaryNode node2 = new NaryNode(2, null);
+        NaryNode node3 = new NaryNode();
+        NaryNode node4 = new NaryNode(4, null);
+        NaryNode node5 = new NaryNode(5, null);
+        NaryNode node6 = new NaryNode(6, null);
+
+        List<NaryNode> node1Children = new ArrayList<NaryNode>();
+        node1Children.add(node3);
+        node1Children.add(node2);
+        node1Children.add(node4);
+
+        List<NaryNode> node3Children = new ArrayList<NaryNode>();
+        node3Children.add(node5);
+        node3Children.add(node6);
+
+        node1.val = 1;
+        node1.children = node1Children;
+        node3.val = 3;
+        node3.children = node3Children;
+
+        NaryTreePreorderRecursive naryTreePreorderRecursive = new NaryTreePreorderRecursive();
+        System.out.println(naryTreePreorderRecursive.preorder(node1));
+
+    }
 }
