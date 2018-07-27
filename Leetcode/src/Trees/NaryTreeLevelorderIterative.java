9b3df5d2e7f5daecc0a4df9af0fab7e1ac2e79e6
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 18:12:40 -0700

    Nary Tree LevelOrder traverse main method

--- Leetcode/src/Trees/NaryTreeLevelorderIterative.java
+++ Leetcode/src/Trees/NaryTreeLevelorderIterative.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 
 public class NaryTreeLevelorderIterative {
 
-    public List<List<Integer>> levelOrder(NaryNode root) {
+    public List<List<Integer>> levelorder(NaryNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         Queue<NaryNode> queue = new LinkedList<>();
 
@@ -30,5 +30,32 @@ public class NaryTreeLevelorderIterative {
             ans.add(subAns);
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        NaryNode node1 = new NaryNode();
+        NaryNode node2 = new NaryNode(2, null);
+        NaryNode node3 = new NaryNode();
+        NaryNode node4 = new NaryNode(4, null);
+        NaryNode node5 = new NaryNode(5, null);
+        NaryNode node6 = new NaryNode(6, null);
+
+        List<NaryNode> node1Children = new ArrayList<NaryNode>();
+        node1Children.add(node3);
+        node1Children.add(node2);
+        node1Children.add(node4);
+
+        List<NaryNode> node3Children = new ArrayList<NaryNode>();
+        node3Children.add(node5);
+        node3Children.add(node6);
+
+        node1.val = 1;
+        node1.children = node1Children;
+        node3.val = 3;
+        node3.children = node3Children;
+
+        NaryTreeLevelorderIterative naryTreeLevelorderIterative = new NaryTreeLevelorderIterative();
+        System.out.println(naryTreeLevelorderIterative.levelorder(node1));
+
     }
 }
