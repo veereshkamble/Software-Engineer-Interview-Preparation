d2db60cd30905055229cbcac5098056022d89813
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 18:10:59 -0700

    Nary Tree LevelOrder traverse

--- Leetcode/src/Trees/NaryTreeLevelorderIterative.java
+++ Leetcode/src/Trees/NaryTreeLevelorderIterative.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class NaryInorderIterative {
+public class NaryTreeLevelorderIterative {
 
     public List<List<Integer>> levelOrder(NaryNode root) {
         List<List<Integer>> ans = new ArrayList<>();
