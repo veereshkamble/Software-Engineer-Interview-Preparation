ef1ac22f7beb0a893f7742a47f906097d1001ca5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 19:19:46 -0700

    N-ary tree maximum depth top down approach main method

--- Leetcode/src/Trees/NaryTreeMaxDepthTopDown.java
+++ Leetcode/src/Trees/NaryTreeMaxDepthTopDown.java
@@ -1,5 +1,8 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NaryTreeMaxDepthTopDown {
 
     public int maxDepth(NaryNode root) {
@@ -12,6 +15,33 @@ public class NaryTreeMaxDepthTopDown {
         }
 
         return max;
+
+    }
+
+    public static void main(String[] args) {
+        NaryNode node1 = new NaryNode();
+        NaryNode node2 = new NaryNode(2, null);
+        NaryNode node3 = new NaryNode();
+        NaryNode node4 = new NaryNode(4, null);
+        NaryNode node5 = new NaryNode(5, null);
+        NaryNode node6 = new NaryNode(6, null);
+
+        List<NaryNode> node1Children = new ArrayList<NaryNode>();
+        node1Children.add(node3);
+        node1Children.add(node2);
+        node1Children.add(node4);
+
+        List<NaryNode> node3Children = new ArrayList<NaryNode>();
+        node3Children.add(node5);
+        node3Children.add(node6);
+
+        node1.val = 1;
+        node1.children = node1Children;
+        node3.val = 3;
+        node3.children = node3Children;
+
+        NaryTreeMaxDepthTopDown naryTreeMaxDepthTopDown= new NaryTreeMaxDepthTopDown();
+        System.out.println(naryTreeMaxDepthTopDown.maxDepth(node1));
 
     }
 }
