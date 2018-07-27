1bba0b4980bce66f036367c4165fcb0e4e11564c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 19:14:40 -0700

    N-ary tree maximum depth bottom up approach main method

--- Leetcode/src/Trees/NaryTreeMaxDepthBottomUp.java
+++ Leetcode/src/Trees/NaryTreeMaxDepthBottomUp.java
@@ -1,5 +1,8 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NaryTreeMaxDepthBottomUp {
 
     public int maxDepth(NaryNode root) {
@@ -8,6 +11,7 @@ public class NaryTreeMaxDepthBottomUp {
         }
 
         int max = 0;
+        
         for (NaryNode child : root.children) { //replace left&right to for loop
             int value = maxDepth(child);
 
@@ -16,5 +20,32 @@ public class NaryTreeMaxDepthBottomUp {
             }
         }
         return max +1;
+    }
+
+    public static void main(String[] args) {
+        NaryNode node1 = new NaryNode();
+        NaryNode node2 = new NaryNode(2, null);
+        NaryNode node3 = new NaryNode();
+        NaryNode node4 = new NaryNode(4, null);
+        NaryNode node5 = new NaryNode(5, null);
+        NaryNode node6 = new NaryNode(6, null);
+
+        List<NaryNode> node1Children = new ArrayList<NaryNode>();
+        node1Children.add(node3);
+        node1Children.add(node2);
+        node1Children.add(node4);
+
+        List<NaryNode> node3Children = new ArrayList<NaryNode>();
+        node3Children.add(node5);
+        node3Children.add(node6);
+
+        node1.val = 1;
+        node1.children = node1Children;
+        node3.val = 3;
+        node3.children = node3Children;
+
+        NaryTreeMaxDepthBottomUp naryTreeMaxDepthBottomUp = new NaryTreeMaxDepthBottomUp();
+        System.out.println(naryTreeMaxDepthBottomUp.maxDepth(node1));
+
     }
 }
