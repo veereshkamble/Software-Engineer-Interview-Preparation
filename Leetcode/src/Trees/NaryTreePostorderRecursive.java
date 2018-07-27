e45c1bfa31ee72e5acd022d2ad74952b756e8caf
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-26 17:40:53 -0700

    Nary Tree postorder recursive amin method

--- Leetcode/src/Trees/NaryTreePostorderRecursive.java
+++ Leetcode/src/Trees/NaryTreePostorderRecursive.java
@@ -17,7 +17,34 @@ public class NaryTreePostorderRecursive {
             }
         }
         list.add(root.val);
-        
+
         return list;
+    }
+
+    public static void main(String[] args) {
+        NaryNode node1 = new NaryNode();
+        NaryNode node2 = new NaryNode(2, null);
+        NaryNode node3 = new NaryNode();
+        NaryNode node4 = new NaryNode(4, null);
+        NaryNode node5 = new NaryNode(5, null);
+        NaryNode node6 = new NaryNode(6, null);
+
+        List<NaryNode> node1Children = new ArrayList<NaryNode>();
+        node1Children.add(node3);
+        node1Children.add(node2);
+        node1Children.add(node4);
+
+        List<NaryNode> node3Children = new ArrayList<NaryNode>();
+        node3Children.add(node5);
+        node3Children.add(node6);
+
+        node1.val = 1;
+        node1.children = node1Children;
+        node3.val = 3;
+        node3.children = node3Children;
+
+        NaryTreePostorderRecursive naryTreePostorderRecursive = new NaryTreePostorderRecursive();
+        System.out.println(naryTreePostorderRecursive.preorder(node1));
+
     }
 }
