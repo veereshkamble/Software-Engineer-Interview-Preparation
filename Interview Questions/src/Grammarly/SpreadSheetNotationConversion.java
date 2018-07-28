506051f322ec8691a292a5b19431756365c2ce24
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-28 00:31:15 -0700

    SPread Sheet Notation COnversion CLass -convertToSpreadSheetNotation method completed

--- Interview Questions/src/Grammarly/SpreadSheetNotationConversion.java
+++ Interview Questions/src/Grammarly/SpreadSheetNotationConversion.java
@@ -9,7 +9,19 @@ public class SpreadSheetNotationConversion {
 
         ans = k / 702;
 
-        
+        if(k % 702 != 0) {
+            ans++;
+        }
+
+        n = n- (702 * (ans - 1));
+
+        while(n > 0) {
+            n--;
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+        result.insert(0, ans);
+        return result.toString();
     }
 }
 
