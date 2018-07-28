303e566a087ae5b975eeb07e765766799444d5c5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-27 23:10:19 -0700

    Particle Speed Number of collisions

--- Interview Questions/src/Grammarly/ParticleSpeedCollision.java
+++ Interview Questions/src/Grammarly/ParticleSpeedCollision.java
@@ -1,4 +1,19 @@
 package Grammarly;
+import java.util.*;
 
 public class ParticleSpeedCollision {
+
+    //Complete the Collision function below
+    static int collision(List<Integer> speed, int pos) {
+        int numCollisions = 0;
+        int count = 0;
+
+        for(Integer particleSpeed : speed) {
+            if(count < pos) {
+                if(speed.get(pos) < particleSpeed) {
+                    numCollisions++;
+                }
+            } else 
+        }
+    }
 }
