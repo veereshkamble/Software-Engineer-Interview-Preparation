21437c7f3927612127fbe25bf195b82532ec984b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-28 00:23:14 -0700

    Particle Speed Number of collisions main method

--- Interview Questions/src/Grammarly/ParticleSpeedCollision.java
+++ Interview Questions/src/Grammarly/ParticleSpeedCollision.java
@@ -23,5 +23,18 @@ public class ParticleSpeedCollision {
         return numCollisions;
     }
 
-    
+    public static void main(String[] args) {
+        List<Integer> speed = new ArrayList<Integer>();
+        speed.add(6);
+        speed.add(6);
+        speed.add(1);
+        speed.add(5);
+        speed.add(4);
+        speed.add(7);
+        speed.add(8);
+
+        int pos = 2;
+
+        
+    }
 }
