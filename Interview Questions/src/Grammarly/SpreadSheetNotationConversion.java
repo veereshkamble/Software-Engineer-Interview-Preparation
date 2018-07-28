4a36bb94bb9442454f04e9b9c03181c8eadcb60e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-28 00:28:57 -0700

    SPread Sheet Notation COnversion CLass -convertToSpreadSheetNotation method

--- Interview Questions/src/Grammarly/SpreadSheetNotationConversion.java
+++ Interview Questions/src/Grammarly/SpreadSheetNotationConversion.java
@@ -1,7 +1,16 @@
 package Grammarly;
 
 public class SpreadSheetNotationConversion {
-    
+
+    public String convertToSpreadSheetNotation(long n) {
+        StringBuilder result = new StringBuilder();
+        long k = n;
+        long ans = 1;
+
+        ans = k / 702;
+
+        
+    }
 }
 
 
