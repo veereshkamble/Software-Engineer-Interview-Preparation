ec900fe5a8bb6e7d70dcaa8ee23b2de28e81bad3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-30 20:48:20 -0700

    Wealth Front Demo Test bug fixes all but one i guess

--- Interview Questions/src/WealthFront/SmallestPositiveInteger.java
+++ Interview Questions/src/WealthFront/SmallestPositiveInteger.java
@@ -30,7 +30,7 @@ public class SmallestPositiveInteger {
             }
         }
 
-        return fullFlag == 1 ? A[A.length -1] + 1 : 1;
+        return fullFlag == 1 ? max + 1 : 1;
     }
     public static void main(String[] args) {
         int[] A = new int[] {3, 2, 1};
