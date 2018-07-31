3b93db9661562053856883b241cde26989e3f465
Veeresh Kamble <veereshkamble7@gmail.com>
2018-07-30 20:29:20 -0700

    Wealth Front Demo Test

---
+++ Interview Questions/src/WealthFront/SmallestPositiveInteger.java
@@ -0,0 +1,28 @@
+package WealthFront;
+import java.util.*;
+
+public class SmallestPositiveInteger {
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int min = Integer.MIN_VALUE;
+
+        Set<Integer> hashSet = new HashSet<>();
+
+        for(int i = 0; i < A.length; i++) {
+            if(A[i] > 0 && !hashSet.contains(A[i])) {
+                hashSet.add(A[i]);
+            }
+        }
+
+        for( int i = 1; i <= A.length; i++) {
+            if(!hashSet.contains(A[i])) {
+                return i;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
