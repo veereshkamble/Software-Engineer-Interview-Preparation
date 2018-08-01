c04e1cba6e57e9d30866991ad98571b3ee643bcd
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-01 12:22:57 -0700

    Contains Duplicates mainmethod

--- Leetcode/src/HashTables/ContainsDuplicates.java
+++ Leetcode/src/HashTables/ContainsDuplicates.java
@@ -15,4 +15,12 @@ public class ContainsDuplicates {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,3,4,5,6,7,7};
+
+        ContainsDuplicates containsDuplicates = new ContainsDuplicates();
+        System.out.println(containsDuplicates.containsDuplicate(nums));
+
+    }
 }
