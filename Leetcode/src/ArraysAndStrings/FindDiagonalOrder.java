6862d17ee589e4707040bdb9f30b97ef84558cb1
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-01 12:50:40 -0700

    Revising problems for Oracle phone interview

--- Leetcode/src/ArraysAndStrings/FindDiagonalOrder.java
+++ Leetcode/src/ArraysAndStrings/FindDiagonalOrder.java
@@ -28,7 +28,7 @@ public class FindDiagonalOrder {
 
     public static void main(String[] args) {
         FindDiagonalOrder findDiagonalOrder = new FindDiagonalOrder();
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
         System.out.println(Arrays.toString(findDiagonalOrder.findDiagonalOrder(matrix)));
     }
 }
