a7219b24563f6e0d9b45ce56a7b0aefc9c4a1687
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-02 12:06:22 -0700

    Oracle Phone Interview Question

---
+++ Interview Questions/src/Oracle/PhoneInterview.java
@@ -0,0 +1,26 @@
+package Oracle;
+import java.io.*;
+import java.util.*;
+
+public class PhoneInterview {
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+/*
+1. Anagram example: abc == cba, aab == aba
+2. Given 2 strings, s and p, find all anagrams of p in s.
+Input:  s: "cbaebabacd" p: "abc"
+Output: [0, 6] cba, bac
+
+*/
+
+}
+
+
+    
+
