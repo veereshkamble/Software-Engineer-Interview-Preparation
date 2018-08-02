d216e23c1521985bb47ab9ef0e43821a68fbb595
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-02 12:28:57 -0700

    Oracle - Anagram index position, starting with HashMap

--- Interview Questions/src/Oracle/PhoneInterview.java
+++ Interview Questions/src/Oracle/PhoneInterview.java
@@ -18,7 +18,22 @@ Input:  s: "cbaebabacd" p: "abc"
 Output: [0, 6] cba, bac
 
 */
+    public int[] anagrams(String s, String p) {
+        char[] stringS = s.toCharArray();
+        char[] stringP = p.toCharArray();
+        Map<Character, Integer> hashMap = new HashMap<Character, Integer>();
+        int[] result = new int[stringS.length];
 
+        for (int i = 0; i < stringP.length; i++) {
+            if (hashMap.containsKey(stringP[i])) {
+                int count = hashMap.get(stringP[i]);
+                count++;
+                hashMap.put(stringP[i], count);
+            } else {
+                hashMap.put(stringP[i], 1);
+            }
+        }
+    }
 }
 
 
