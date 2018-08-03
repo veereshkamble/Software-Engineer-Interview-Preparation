6e6c59c9579074e94d887f3e03e431ebe703de1f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-03 13:54:17 -0700

    Sliding WIndow template for string problems corner cases

--- Leetcode/src/SlidingWindowAlgorithm/SlidingWindowTemplate.java
+++ Leetcode/src/SlidingWindowAlgorithm/SlidingWindowTemplate.java
@@ -4,6 +4,13 @@ import java.util.*;
 public class SlidingWindowTemplate {
 
     public List<Integer> slidingWindowTemplate(String s, String t) {
+        //Initialize a collection or an int value to save the result according to the question
+        List<Integer> result = new LinkedList<>();
+
+        if(t.length() > s.length()) {
+            return result;
+        }
+
         
     }
 }
