340b5d8e1b61e3449cdcdae53dee9a5a3bf32e43
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-04 16:37:45 -0700

    Queue implementation using Arraylist data structure - enqueue() and dequeue() mehtods

--- Leetcode/src/Queues/MyQueue.java
+++ Leetcode/src/Queues/MyQueue.java
@@ -13,4 +13,21 @@ public class MyQueue {
         data = new ArrayList<Integer>();
         p_start = 0;
     }
+
+    //insert an element into the queue. Return true if the operation is successfull
+    public boolean enQueue(int x) {
+        data.add(x);
+        return true;
+    }
+
+    //delete an element from the queue. Return true if the operation is successfull
+    public boolean deQueue() {
+        if(isEmpty() == true) {
+            return false;
+        }
+        p_start++;
+        return true;
+    }
+
+    
 }
