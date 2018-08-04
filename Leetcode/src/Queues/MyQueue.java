827c7164019f9bd802c9adb9f1c6832b62140cf6
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-04 16:46:23 -0700

    Queue implementation using Arraylist data structure - main mehtod()

--- Leetcode/src/Queues/MyQueue.java
+++ Leetcode/src/Queues/MyQueue.java
@@ -39,5 +39,20 @@ public class MyQueue {
         return p_start >= data.size();
     }
 
-    
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.enQueue(5);
+        q.enQueue(3);
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+        q.deQueue();
+        if (q.isEmpty() == false) {
+            System.out.println(q.Front());
+        }
+    }
 }
