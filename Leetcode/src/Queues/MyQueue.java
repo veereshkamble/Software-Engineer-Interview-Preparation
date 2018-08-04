fd53f7321268e925ed7031347ed2d816d8c52e70
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-04 16:34:09 -0700

    Queue implementation using Arraylist data structure

--- Leetcode/src/Queues/MyQueue.java
+++ Leetcode/src/Queues/MyQueue.java
@@ -1,6 +1,16 @@
 package Queues;
+import  java.util.*;
 
 public class MyQueue {
 
-    
+    //store elements
+    private List<Integer> data;
+
+    //a pointer to indicate the starting position
+    private int p_start;
+
+    public MyQueue() {
+        data = new ArrayList<Integer>();
+        p_start = 0;
+    }
 }
