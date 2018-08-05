78dd23c7c302ac7b3e5b58443efdba8325419e6b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-05 14:08:13 -0700

    Moving average from data stream

---
+++ Leetcode/src/Queues/MovingAverage.java
@@ -0,0 +1,26 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverage {
+
+    private double previosSum = 0.0;
+    private int maxSixe;
+    private Queue<Integer> queue;
+
+    public MovingAverage(int size) {
+        queue = new LinkedList<Integer>();
+        maxSixe = size;
+    }
+
+    public double next(int val) {
+        if(queue.size() == maxSixe) {
+            previosSum -= queue.remove();
+        }
+
+        previosSum += val;
+        queue.add(val);
+        return previosSum / queue.size();
+    }
+}
