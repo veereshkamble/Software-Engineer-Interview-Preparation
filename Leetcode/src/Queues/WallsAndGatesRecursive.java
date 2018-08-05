35566fddf9389a80a311dc6122930fd61b14022b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-05 16:31:52 -0700

    Walls and Gates Recursive

---
+++ Leetcode/src/Queues/WallsAndGatesRecursive.java
@@ -0,0 +1,21 @@
+package Queues;
+
+public class WallsAndGatesRecursive {
+
+    public void wallsAndGates(int[][] rooms) {
+        for (int i = 0; i < rooms.length; i++)
+            for (int j = 0; j < rooms[0].length; j++)
+                if (rooms[i][j] == 0) dfs(rooms, i, j, 0);
+    }
+
+    private void dfs(int[][] rooms, int i, int j, int d) {
+        if (i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < d) return;
+        rooms[i][j] = d;
+        dfs(rooms, i - 1, j, d + 1);
+        dfs(rooms, i + 1, j, d + 1);
+        dfs(rooms, i, j - 1, d + 1);
+        dfs(rooms, i, j + 1, d + 1);
+    }
+
+    
+}
