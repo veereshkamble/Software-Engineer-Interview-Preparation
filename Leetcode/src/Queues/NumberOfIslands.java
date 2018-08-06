6e20d5aae7753b0e4dca1fbc7aa81a8d75e8c030
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-05 19:35:24 -0700

    Number of Islands main method

--- Leetcode/src/Queues/NumberOfIslands.java
+++ Leetcode/src/Queues/NumberOfIslands.java
@@ -33,7 +33,16 @@ public class NumberOfIslands {
     public static void main(String[] args) {
         NumberOfIslands numberOfIslands = new NumberOfIslands();
         char[][] grid = new char[][] {{'1','1','0','0','0'},
-                                      {'1','1','0','1','0'},
-                                      {   }}
-    }
+                                      {'1','1','0','0','0'},
+                                      {'0','0','1','0','0'},
+                                      {'0','0','0','1','1'}};
+
+        System.out.println(numberOfIslands.numOfIslands(grid));
+        /*for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+        }
 }
