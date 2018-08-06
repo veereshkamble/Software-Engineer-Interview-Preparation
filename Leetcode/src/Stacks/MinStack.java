483be3a5d153e092df73950fadd40ae9b20b50f1
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-06 13:45:02 -0700

    Min Stack - main()

--- Leetcode/src/Stacks/MinStack.java
+++ Leetcode/src/Stacks/MinStack.java
@@ -34,4 +34,17 @@ public class MinStack {
     public int getMin() {
         return min;
     }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(5);
+        minStack.push(4);
+        minStack.push(6);
+        minStack.push(3);
+        System.out.println(minStack.getMin());
+        System.out.println(minStack.top());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+        System.out.println(minStack.top());
+    }
 }
