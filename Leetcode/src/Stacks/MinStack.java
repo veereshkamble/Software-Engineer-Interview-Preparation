c740d1e6fd238b6a647fc42823a18f322065a1cf
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-06 13:36:33 -0700

    Min Stack - Initaializing data structure

--- Leetcode/src/Stacks/MinStack.java
+++ Leetcode/src/Stacks/MinStack.java
@@ -1,13 +1,19 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class MinStack {
+    int min;
+    Stack<Integer> stack;
 
     /** initialize your data structure here. */
     public MinStack() {
-
+        min = Integer.MAX_VALUE;
+        stack = new Stack<Integer>();
     }
 
     public void push(int x) {
+        
 
     }
 
