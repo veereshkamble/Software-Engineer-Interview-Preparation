2017b0ec39fe0b28eb274af99749de9bdefc16dd
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-06 12:16:41 -0700

    Stacks - A LIFO data structure

---
+++ Leetcode/src/Stacks/MyStack.java
@@ -0,0 +1,15 @@
+package Stacks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStack {
+
+    private List<Integer> data;
+
+    public MyStack() {
+        data = new ArrayList<>();
+    }
+
+    
+}
