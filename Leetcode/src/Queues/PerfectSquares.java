f33cb72a8d8d943f5f2a184ed12d99d28462eb4b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-05 20:24:44 -0700

    Perfect squares using dynamic programming

---
+++ Leetcode/src/Queues/PerfectSquares.java
@@ -0,0 +1,18 @@
+package Queues;
+
+public class PerfectSquares {
+
+    public int numSquares(int n) {
+        int[] DP = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            int min= Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++)
+            {
+                min= Math.min(min, DP[i - j * j] + 1);
+            }
+            DP[i] = min;
+        }
+        return DP[n];
+    }
+}
