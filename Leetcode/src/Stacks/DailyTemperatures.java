2283242c0837a5b5dacf2c3b3ab59251a52af118
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-06 15:51:48 -0700

    Daily Temperatures optimized using Stack

--- Leetcode/src/Stacks/DailyTemperatures.java
+++ Leetcode/src/Stacks/DailyTemperatures.java
@@ -1,21 +1,31 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class DailyTemperatures {
 
     public int[] dailyTemperatures(int[] temperatures) {
-
+        Stack<Integer> stack = new Stack<Integer>();
         int[] result = new int[temperatures.length];
 
         for(int i = 0; i < temperatures.length; i++) {
-            int days = 0;
-            for(int j = i + 1; j < temperatures.length; j++) {
-                days++;
-                if(temperatures[j] > temperatures[i]) {
-                    result[i] = days;
-                    break;
-                }
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
             }
+            stack.push(i);
         }
+
         return result;
+    }
+
+    public static void main(String[] args) {
+        DailyTemperatures dailyTemperatures = new DailyTemperatures();
+        int[] temperatures = new int[] {73, 78, 75, 71, 69, 72, 76, 73};
+        int[] result = dailyTemperatures.dailyTemperatures(temperatures);
+
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
     }
 }
