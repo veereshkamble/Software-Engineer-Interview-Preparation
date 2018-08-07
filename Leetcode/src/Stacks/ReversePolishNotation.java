2937bc8ab3135fc5b790894520a5e2c0dda146c1
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-06 17:47:36 -0700

    Reverse Polish Notation

--- Leetcode/src/Stacks/ReversePolishNotation.java
+++ Leetcode/src/Stacks/ReversePolishNotation.java
@@ -1,4 +1,40 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class ReversePolishNotation {
+
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int opA = 0;
+        int opB = 0;
+        for(String s : tokens) {
+            if(s.equals("+")) {
+                opB = stack.pop();
+                opA = stack.pop();
+                result = opA + opB;
+                stack.push(result);
+            } else if(s.equals("-")) {
+                opB = stack.pop();
+                opA = stack.pop();
+                result = opA - opB;
+                stack.push(result);
+            } else if(s.equals("*")) {
+                opB = stack.pop();
+                opA = stack.pop();
+                result = opA * opB;
+                stack.push(result);
+            } else if(s.equals("/")) {
+                opB = stack.pop();
+                opA = stack.pop();
+                result = opA / opB;
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
 }
