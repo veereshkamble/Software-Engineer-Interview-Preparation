5eea4783c86871359621bb268ecdada171e41d87
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-07 13:57:01 -0700

    Implementing a Queue data struture using a stack

--- Leetcode/src/StacksAndQueues/QueueUsingStack.java
+++ Leetcode/src/StacksAndQueues/QueueUsingStack.java
@@ -1,28 +1,42 @@
 package StacksAndQueues;
 
-    public class QueueUsingStack {/** Initialize your data structure here. */
-    public QueueUsingStack() {
+import java.util.Stack;
 
-    }
+public class QueueUsingStack {
+
+        Stack<Integer> input;
+        Stack<Integer> output;
+
+        /** Initialize your data structure here. */
+        public QueueUsingStack() {
+            input = new Stack<Integer>();
+            output = new Stack<Integer>();
+        }
 
         /** Push element x to the back of queue. */
         public void push(int x) {
-
+            input.push(x);
         }
 
         /** Removes the element from in front of queue and returns that element. */
         public int pop() {
-
+            peek();
+            return output.pop();
         }
 
         /** Get the front element. */
         public int peek() {
-
+            if(output.isEmpty()) {
+                while(!input.isEmpty()) {
+                    output.push(input.pop());
+                }
+            }
+            return output.peek();
         }
 
         /** Returns whether the queue is empty. */
         public boolean empty() {
-
+            return input.isEmpty() && output.isEmpty();
         }
     }
 
