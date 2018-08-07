3ac0e55aaf26a62b9861b53d5e5c5dcb65d9a9e0
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-07 16:39:56 -0700

    Decode String

---
+++ Leetcode/src/StacksAndQueues/DecodeString.java
@@ -0,0 +1,39 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+public class DecodeString {
+
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<Integer>();
+        Stack<String> charStack = new Stack<String>();
+
+        int i = 0;
+        while(i < s.length()) {
+            char ch = s.charAt(i);
+
+            if(ch >= '0' && ch <= '9') {
+                int start = i;
+                while(s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9') {
+                    i++;
+                }
+                numStack.push(Integer.parseInt(s.substring(start, i+1)));
+            } else if(ch == '[') {
+                charStack.push("");
+            } else if(ch == ']') {
+                String str = charStack.pop();
+                int times = numStack.pop();
+                StringBuilder sb = new StringBuilder();
+
+                for(int j = 0; j < times; j++) {
+                    sb.append(str);
+                }
+                charStack.push(charStack.pop() + sb.toString());
+            } else {
+                charStack.push(charStack.pop() + ch);
+            }
+            i = i + 1;
+        }
+        return charStack.pop();
+    }
+}
