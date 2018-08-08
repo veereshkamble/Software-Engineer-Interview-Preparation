42f25532165b957384ada8149ee04278262eb4d2
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-08 16:25:19 -0700

    Staring with Tries

---
+++ Leetcode/src/Tries/TrieNodeArray.java
@@ -0,0 +1,9 @@
+package Tries;
+
+public class TrieNodeArray {
+    // change this value to adapt to different cases
+    public static final int N = 26;
+    public TrieNodeArray[] children = new TrieNodeArray[N];
+
+    // you might need some extra values according to different cases
+}
