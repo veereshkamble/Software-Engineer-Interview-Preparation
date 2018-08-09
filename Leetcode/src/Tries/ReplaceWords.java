166358a75a22c1558aebc7446c3f64e296dbde64
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-09 14:41:51 -0700

    Replace words using trie

--- Leetcode/src/Tries/ReplaceWords.java
+++ Leetcode/src/Tries/ReplaceWords.java
@@ -4,8 +4,51 @@ import java.util.List;
 
 public class ReplaceWords {
 
-    public String repalceWords(List<String> dict, String sentence) {
+    public String replaceWords(List<String> dict, String sentence) {
         String[] tokens = sentence.split(" ");
-        TrieNode
+        TrieNode trieNode = buildTrie(dict);
+        return replaceWords(tokens, trieNode);
+    }
+
+    public TrieNode buildTrie(List<String> dict) {
+        TrieNode root = new TrieNode();
+        for(String rootWord : dict) {
+            TrieNode cur = root;
+            for(int i = 0 ; i < rootWord.length(); i++) {
+                char c = rootWord.charAt(i);
+                if(cur.childrenMap.get(c) == null) {
+                    cur.childrenMap.put(c, new TrieNode());
+                }
+                cur = cur.childrenMap.get(c);
+            }
+            cur.isWord = true;
+        }
+        return root;
+    }
+
+    public String replaceWords(String[] tokens, TrieNode root) {
+        StringBuilder sb = new StringBuilder();
+        for(String token : tokens) {
+            sb.append(getShortestReplacement(token, root));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public String getShortestReplacement(String token, TrieNode root) {
+        TrieNode cur = root;
+        StringBuilder sb = new StringBuilder();
+        for(char c : token.toCharArray()) {
+            sb.append(c);
+            if(cur.childrenMap.get(c) != null) {
+                if(cur.childrenMap.get(c).isWord == true) {
+                    return sb.toString();
+                }
+                cur = cur.childrenMap.get(c);
+            } else {
+                return token;
+            }
+        }
+        return token;
     }
 }
