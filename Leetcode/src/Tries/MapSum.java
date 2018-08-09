10d136d0abf3d10224dccd0981bb559d0bf19c8a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-08 20:55:14 -0700

    Map Sum - insert()

--- Leetcode/src/Tries/MapSum.java
+++ Leetcode/src/Tries/MapSum.java
@@ -1,4 +1,30 @@
 package Tries;
 
+import java.util.Map;
+
 public class MapSum {
+
+    TrieNode root;
+
+    public MapSum() {
+        root = new TrieNode();
+    }
+
+    public void insert(String key, int val) {
+        TrieNode cur = root;
+        for(char c : key.toCharArray()) {
+            TrieNode next = cur.childrenMap.get(c);
+            if(next == null) {
+                next = new TrieNode();
+                cur.childrenMap.put(c, next);
+            }
+            cur = next;
+        }
+        cur.isWord = true;
+        cur.val = val;
+    }
+
+    
+
+
 }
