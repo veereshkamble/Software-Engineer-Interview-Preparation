07d90bb0fe28edfbb95afdf3139ce113b346903a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-08 18:17:47 -0700

    Implementing Trie - startsWith method()

--- Leetcode/src/Tries/Trie.java
+++ Leetcode/src/Tries/Trie.java
@@ -37,7 +37,15 @@ class Trie {
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-
+        TrieNode cur = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if(cur.childrenMap.get(c) == null) {
+                return false;
+            }
+            cur = cur.childrenMap.get(c);
+        }
+        return true;
     }
 }
 
