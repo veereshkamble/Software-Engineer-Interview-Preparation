104fa56bbfd57f75136ae7303f2dfce3541ecf51
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-09 21:39:33 -0700

    Word Dictionary - search method()

--- Leetcode/src/Tries/WordDictionary.java
+++ Leetcode/src/Tries/WordDictionary.java
@@ -53,6 +53,18 @@ class WordDictionary {
 
         return false;
     }
+
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        System.out.println(wordDictionary.search("pad"));
+        System.out.println(wordDictionary.search("bad"));
+        System.out.println(wordDictionary.search(".ad"));
+        System.out.println(wordDictionary.search("b.."));
+
+    }
 }
 
 /**
