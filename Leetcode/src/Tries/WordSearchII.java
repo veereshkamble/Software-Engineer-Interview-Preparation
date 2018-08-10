72ddcf9a6584a156f664b163c775056eb081b78f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-10 11:57:21 -0700

    Word Search II - findWOrds()

--- Leetcode/src/Tries/WordSearchII.java
+++ Leetcode/src/Tries/WordSearchII.java
@@ -8,6 +8,10 @@ public class WordSearchII {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<String>();
         TrieNode root = buildTrie(words);
-        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
     }
 }
