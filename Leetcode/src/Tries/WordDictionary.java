76b225f9114b01b822e7244e079b9d855e05e180
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-09 19:33:46 -0700

    Word Dictionary - add() method

--- Leetcode/src/Tries/WordDictionary.java
+++ Leetcode/src/Tries/WordDictionary.java
@@ -2,14 +2,22 @@ package Tries;
 
 class WordDictionary {
 
+    TrieNode root;
     /** Initialize your data structure here. */
     public WordDictionary() {
-
+        root = new TrieNode();
     }
 
     /** Adds a word into the data structure. */
     public void addWord(String word) {
-
+        TrieNode cur = root;
+        for(char c : word.toCharArray()) {
+            if(cur.childrenMap.get(c) == null) {
+                cur.childrenMap.put(c, new TrieNode());
+            }
+            cur = cur.childrenMap.get(c);
+        }
+        cur.isWord = true;
     }
 
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
