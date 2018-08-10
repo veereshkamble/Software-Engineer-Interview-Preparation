0c02c77dfe3e2607799afdfc962d97ec277ea71d
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-09 18:01:43 -0700

    Search Auto Complete System

--- Leetcode/src/Tries/AutoCompleteSystem.java
+++ Leetcode/src/Tries/AutoCompleteSystem.java
@@ -1,6 +1,17 @@
 package Tries;
 
 public class AutoCompleteSystem {
+    TrieNode root;
+    String prefix;
 
+    public AutoCompleteSystem(String[] sentences, int[] times) {
+        root = new TrieNode();
+        prefix = " ";
+        for(int i = 0; i < sentences.length; i++) {
+            buildTrie(sentences[i], times[i]);
+        }
+    }
+
+    
 
 }
