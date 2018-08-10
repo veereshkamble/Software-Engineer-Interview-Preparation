4202d05416c283fef16d005a13419833689242ec
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-10 15:29:42 -0700

    Maximum XOR of Two numbers

--- Leetcode/src/Tries/MaximumXOR.java
+++ Leetcode/src/Tries/MaximumXOR.java
@@ -19,7 +19,21 @@ public class MaximumXOR {
             }
         }
 
-        
-
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            TrieNodeXOR curNode = root;
+            int curSum = 0;
+            for(int i = 31; i >= 0; i--) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.childrenMap.get(curBit) != null) {
+                    curSum += (1 << i);
+                    curNode = curNode.childrenMap.get(curBit);
+                } else {
+                    curNode = curNode.childrenMap.get(curBit);
+                }
+            }
+            max = Math.max(curSum, max);
+        }
+        return max;
     }
 }
