db0074bdb90d30dd353349af44e3a308b27b4b33
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-10 17:12:04 -0700

    Palindrome Pairs using Trie

--- Leetcode/src/Tries/PalindromePairs.java
+++ Leetcode/src/Tries/PalindromePairs.java
@@ -1,4 +1,23 @@
 package Tries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PalindromePairs {
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        TrieNode root = new TrieNode();
+        for(int i = 0; i < words.length; i++) {
+            buildTrie(root, words[i], i);
+        }
+
+        for(int i = 0; i < words.length; i++) {
+            searchTrie(words, i, root, result);
+        }
+        return result;
+    }
+
+    
 }
