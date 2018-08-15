6ad0707a6d00045646458ef0f338310bcc5f7b44
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-14 21:46:23 -0700

    Review of Sorting - Bubble Sort Implementation - swap method()

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/BubbleSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/BubbleSort.java
@@ -1,4 +1,19 @@
 package com.interviewprep.sortingalgorithms;
 
 public class BubbleSort {
+
+    public static void swap(int[] array, int i, int j) {
+
+        if(i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
