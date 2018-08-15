1a72da94ea68f7859a828e07717b7f95f5629e62
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-15 12:28:38 -0700

    Selection Sort

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/SelectionSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/SelectionSort.java
@@ -1,4 +1,24 @@
 package com.interviewprep.sortingalgorithms;
 
 public class SelectionSort {
+
+    public static void swap(int[] array, int i, int j) {
+        if(i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static  void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+    }
 }
