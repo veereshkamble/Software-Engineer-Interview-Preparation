5fd06f096e78a21347c8a093c6c2c84e121230a5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-15 13:15:51 -0700

    Insertion sort()

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/InsertionSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/InsertionSort.java
@@ -5,10 +5,21 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
-        for(int i =1; i < intArray.length; i++)
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+
+            for(i = firstUnsortedIndex; i > 0 && intArray[i-1] > newElement; i--) {
+                intArray[i] = intArray[i-1];
+            }
+            intArray[i] = newElement;
+
+
+
+        }
 
         for(int i = 0; i < intArray.length; i++) {
-            System.out.print(intArray[i]);
+            System.out.print(intArray[i] + " ");
         }
     }
 }
