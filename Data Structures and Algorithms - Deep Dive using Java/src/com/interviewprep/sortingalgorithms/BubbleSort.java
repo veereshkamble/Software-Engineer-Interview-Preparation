bd06d69fd6b599525c55adacfa71ec42e0781114
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-14 21:53:56 -0700

    Review of Sorting - Bubble Sort Implementation - main()

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/BubbleSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex++) {
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             for(int i = 0; i < lastUnsortedIndex; i++) {
                 if(intArray[i] > intArray[i+1]) {
                     swap(intArray, i, i+1);
@@ -24,6 +24,8 @@ public class BubbleSort {
             }
         }
 
-        
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
     }
 }
