929be51f938222588ce2b7ff30b8d379fb87f965
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-15 12:33:21 -0700

    Selection Sort

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/SelectionSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/SelectionSort.java
@@ -15,7 +15,16 @@ public class SelectionSort {
     public static  void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        for(int lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int maxIndex = 0;
 
+            for(int i = 1; i <= lastUnsortedIndex; i++) {
+                if(intArray[i] > intArray[maxIndex]) {
+                    maxIndex = i;
+                }
+                swap(intArray, maxIndex, lastUnsortedIndex);
+            }
+        }
 
         for(int i = 0; i < intArray.length; i++) {
             System.out.print(intArray[i] + " ");
