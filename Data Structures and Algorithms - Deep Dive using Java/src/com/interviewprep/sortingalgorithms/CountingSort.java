07cdeb7f760f5e1688cc2e4cb0e10652fa4730bc
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-16 12:56:33 -0700

    Counting sort main()

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/CountingSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/CountingSort.java
@@ -2,5 +2,12 @@ package com.interviewprep.sortingalgorithms;
 
 public class CountingSort {
 
-    
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+    }
 }
