59d8cedf4f9855b2185c40b53d4ac8e6903260b5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-16 14:17:10 -0700

    Radix Sort - radix SIngle sort()

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/RadixSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/RadixSort.java
@@ -2,7 +2,11 @@ package com.interviewprep.sortingalgorithms;
 
 public class RadixSort {
 
-
+    public static void radixSort(int[] input, int radix, int width) {
+        for(int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
 
     public static void main(String[] args) {
         int[] radixSort = {4725, 4586, 1330, 8792, 1594, 5729 };
