3f021fa83fba52e0e1571ddb0b12529ff35daa2b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-16 14:29:24 -0700

    Radix sort stable"

---
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/RadixSortStable.java
@@ -0,0 +1,42 @@
+package com.interviewprep.sortingalgorithms;
+
+public class RadixSortStable {
+
+    public static void radixSort(int[] input, int radix, int width) {
+        for(int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(int[] input, int position, int radix) {
+        int numItems = input.length;
+
+        int[] countArray = new int[radix];
+
+        for(int value : input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+
+        //Adjust the count array
+        for(int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j-1];
+        }
+
+        int[] temp = new int[numItems];
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+        }
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(10, position) % radix;
+    }
+
+    public static void main(String[] args) {
+        int[] radixSort = {4725, 4586, 1330, 8792, 1594, 5729 };
+
+        for(int i = 0; i < radixSort.length; i++) {
+            System.out.print(radixSort[i] + " ");
+        }
+    }
+}
