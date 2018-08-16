a225de13db071d99a349fdb76a51d429e0969a11
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-15 19:25:16 -0700

    Merge Sort

---
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/MergeSort.java
@@ -0,0 +1,15 @@
+package com.interviewprep.sortingalgorithms;
+
+public class MergeSort {
+
+    
+
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
