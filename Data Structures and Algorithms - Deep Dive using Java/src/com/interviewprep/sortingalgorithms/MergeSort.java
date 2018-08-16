b7f712ff45414d869347ca07ff13f9338a9baa8d
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-15 19:36:00 -0700

    Merge Sort

--- Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/MergeSort.java
+++ Data Structures and Algorithms - Deep Dive using Java/src/com/interviewprep/sortingalgorithms/MergeSort.java
@@ -2,7 +2,17 @@ package com.interviewprep.sortingalgorithms;
 
 public class MergeSort {
 
-    
+    public static void mergeSort(int[] input, int start, int end) {
+
+        if(end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
 
 
     public static void main(String[] args) {
