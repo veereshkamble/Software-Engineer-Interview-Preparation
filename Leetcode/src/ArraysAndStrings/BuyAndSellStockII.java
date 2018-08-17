267a8acb157db343399b4f089136e47fe53daf5a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-16 21:49:41 -0700

    Buy and Sell Stock II - main method()

--- Leetcode/src/ArraysAndStrings/BuyAndSellStockII.java
+++ Leetcode/src/ArraysAndStrings/BuyAndSellStockII.java
@@ -10,4 +10,12 @@ public class BuyAndSellStockII {
 
         return total;
     }
+
+    public static  void main(String[] args) {
+        int[]  prices = new int[] {7, 1, 5, 3, 6, 4};
+
+        BuyAndSellStockII buyAndSellStockII = new BuyAndSellStockII();
+        System.out.println(buyAndSellStockII.maxProfit(prices));
+    }
+
 }
