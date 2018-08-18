c833a8eb68c5bff9214da4de14684d1feb71187c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-17 21:33:22 -0700

    Rotate Matrix - main()

--- Leetcode/src/ArraysAndStrings/RotateMatrix.java
+++ Leetcode/src/ArraysAndStrings/RotateMatrix.java
@@ -37,6 +37,18 @@ public class RotateMatrix {
     }
 
     public static void main(String[] args) {
+        int[][] matrix = new int[][] { {1,2,3},
+                {4,5,6},
+                {7,8,9}};
 
+        RotateMatrix rotateMatrix = new RotateMatrix();
+        rotateMatrix.rotate(matrix);
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
