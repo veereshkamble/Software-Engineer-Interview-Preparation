88506d638bf30220a353afa9413a0b447e9f6856
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-17 21:24:08 -0700

    Rotate Matirx

--- Leetcode/src/ArraysAndStrings/RotateMatrix.java
+++ Leetcode/src/ArraysAndStrings/RotateMatrix.java
@@ -2,6 +2,16 @@ package ArraysAndStrings;
 
 public class RotateMatrix {
 
+    public boolean rotate(int[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0 || matrix.length != matrix[0].length) {
+            return false;
+        }
+
+        int n = matrix.length;
+
+        
+
+    }
 
     public static void main(String[] args) {
 
