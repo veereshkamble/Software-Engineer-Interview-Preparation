404d43492fb603736abc4f506de2abc5ba26877d
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-18 19:30:49 -0700

    Number of Islands DFS - numberOfIsands()

--- GoogleInterviewPrep/src/InterviewProcess/NumberOfIslandsDFS.java
+++ GoogleInterviewPrep/src/InterviewProcess/NumberOfIslandsDFS.java
@@ -17,5 +17,23 @@ public class NumberOfIslandsDFS {
         dfs(grid, r, c + 1);
     }
 
-    
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
 }
