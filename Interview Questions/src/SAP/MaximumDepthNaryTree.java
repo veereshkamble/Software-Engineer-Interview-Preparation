e23cfdf190d6e7a26758600838ebf0da34af9b39
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-19 15:08:01 -0700

    SAP SVNT online test - Maximum Depth of N ary Tree

---
+++ Interview Questions/src/SAP/MaximumDepthNaryTree.java
@@ -0,0 +1,28 @@
+package SAP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaximumDepthNaryTree {
+
+    public class Node {
+        int val;
+        List<Node> children = new ArrayList<Node>();
+    }
+
+    public int maxDepth(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int max = 0;
+        for(Node child : root.children) {
+            int value = maxDepth(child);
+
+            if(value > max) {
+                max = value;
+            }
+        }
+        return max + 1;
+    }
+}
