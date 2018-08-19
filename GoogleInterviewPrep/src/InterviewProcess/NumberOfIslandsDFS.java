f7f5e13c1109d16182ac83cf3c057cf8d963b1eb
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-18 19:30:28 -0700

    Number of Islands DFS

---
+++ GoogleInterviewPrep/src/InterviewProcess/NumberOfIslandsDFS.java
@@ -0,0 +1,21 @@
+package InterviewProcess;
+
+public class NumberOfIslandsDFS {
+
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    
+}
