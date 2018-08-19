2ddbb92d9fca4219e3e27ee4c11acdd3ef3c086a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-18 20:57:42 -0700

    Number of Islands - BFS main()

--- GoogleInterviewPrep/src/InterviewProcess/NumberOfIslandsBFS.java
+++ GoogleInterviewPrep/src/InterviewProcess/NumberOfIslandsBFS.java
@@ -50,9 +50,9 @@ public class NumberOfIslandsBFS {
     public static void main(String[] args) {
         NumberOfIslandsBFS numberOfIslandsBFS = new NumberOfIslandsBFS();
         char[][] grid = new char[][] {{'1','1','0','0','0'},
-                {'1','1','0','0','1'},
-                {'0','0','1','0','0'},
-                {'1','0','0','1','1'}};
+                                      {'1','1','0','0','0'},
+                                      {'0','0','1','0','0'},
+                                      {'0','0','0','1','1'}};
 
         System.out.println(numberOfIslandsBFS.numIslands(grid));
     }
