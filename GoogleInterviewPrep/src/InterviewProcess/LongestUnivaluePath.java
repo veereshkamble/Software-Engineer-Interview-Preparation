f41439140c608162fc4a50645455d1baf6776e60
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-20 19:05:00 -0700

    Longest Univalue Path

---
+++ GoogleInterviewPrep/src/InterviewProcess/LongestUnivaluePath.java
@@ -0,0 +1,21 @@
+package InterviewProcess;
+
+public class LongestUnivaluePath {
+
+    
+    int len = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        if(root != null)
+            longestUnivaluePath(root, -1);
+        return len;
+    }
+    private int longestUnivaluePath(TreeNode curr, int val){
+        if(curr == null) return 0;
+        int l = longestUnivaluePath(curr.left, curr.val), r = longestUnivaluePath(curr.right, curr.val), count = 0;
+        len = Math.max(len, l+r); //l is "valid" connecting edges to me from left
+        if(curr.val == val)
+            count = 1+Math.max(l,r); //give it to caller max path with same number, include me
+        return count;
+    }
+
+}
