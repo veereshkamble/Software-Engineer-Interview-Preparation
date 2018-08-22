c8f12ccdebbfbd73a290bf9b03cbac186150245f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-21 23:22:47 -0700

    Google Interview Prep - Arrays and Strings - Valid Parenthesis

---
+++ GoogleInterviewPrep/src/ArraysAndStrings/ValidParenthesis.java
@@ -0,0 +1,31 @@
+package ArraysAndStrings;
+
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public boolean isValidString(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        //Iterate through the string until empty
+        for(int i = 0; i < s.length(); i++) {
+
+            //push any open paranthesis onto the stack
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+}
