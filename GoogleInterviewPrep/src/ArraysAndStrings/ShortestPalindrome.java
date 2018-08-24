4c42c8607c8679132f9e44fecf7b3b2dff2ba47c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-23 19:37:38 -0700

    Google Interview Prep - Arrays and Strings - Shortest Palindrome using KMP algorithm - getPrefixTable()

--- GoogleInterviewPrep/src/ArraysAndStrings/ShortestPalindrome.java
+++ GoogleInterviewPrep/src/ArraysAndStrings/ShortestPalindrome.java
@@ -1,4 +1,25 @@
 package ArraysAndStrings;
 
 public class ShortestPalindrome {
+
+    public int[] getPrefixTable(String s) {
+        int i = 1, j = 0;
+        int n = s.length();
+        int[] table = new int[s.length()];
+
+        while(i < n) {
+            if(s.charAt(i) == s.charAt(j)) {
+                table[i] = j + 1;
+                i++;
+                j++;
+            } else if(j > 0) {
+                j = table[j-1];
+            } else {
+                table[i] = 0;
+                i++;
+            }
+        }
+        return table;
+    }
+    
 }
