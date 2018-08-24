c0291050f37ac4b6939c73ed3d67d73cc64035fb
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-23 19:38:26 -0700

    Google Interview Prep - Arrays and Strings - Shortest Palindrome using KMP algorithm - shortestPalindrome()

--- GoogleInterviewPrep/src/ArraysAndStrings/ShortestPalindrome.java
+++ GoogleInterviewPrep/src/ArraysAndStrings/ShortestPalindrome.java
@@ -2,6 +2,14 @@ package ArraysAndStrings;
 
 public class ShortestPalindrome {
 
+    public String shortestPalindrome(String s) {
+        String temp = s + "#" + new StringBuilder(s).reverse().toString();
+        int[] table = getPrefixTable(temp);
+
+        //get the maximum palin part in s starts from 0
+        return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
+    }
+
     public int[] getPrefixTable(String s) {
         int i = 1, j = 0;
         int n = s.length();
@@ -21,5 +29,5 @@ public class ShortestPalindrome {
         }
         return table;
     }
-    
+
 }
