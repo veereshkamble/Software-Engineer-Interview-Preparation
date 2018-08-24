13f13f64da94319763a4e9b91e09fc781c969c02
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-23 22:49:12 -0700

    Google Interview Prep - Arrays and Strings - Image Smoother()

--- GoogleInterviewPrep/src/ArraysAndStrings/ImageSmoother.java
+++ GoogleInterviewPrep/src/ArraysAndStrings/ImageSmoother.java
@@ -1,4 +1,33 @@
 package ArraysAndStrings;
 
 public class ImageSmoother {
+
+    public int[][] imageSmoother(int[][] M) {
+        if(M == null || M.length == 0) {
+            return M;
+        }
+        int m = M.length;
+        int n = M[0].length;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                M[i][j] = calculateAverage(M, m, n, i, j);
+            }
+        }
+        return M;
+    }
+
+    public int calculateAverage(int[][] M, int m, int n, int i, int j) {
+        int sum = 0;
+        int count = 0;
+        for(int x = Math.max(i-1, 0); x <= Math.min(i+1, m-1); x++) {
+            for(int y = Math.max(j-1, 0); y <= Math.min(j+1, n-1); y++) {
+                sum += M[i][j];
+                count++;
+            }
+        }
+        return sum / count;
+    }
+
+   
 }
