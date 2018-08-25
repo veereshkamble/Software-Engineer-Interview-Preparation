6b471c12a8ed019d9f947015d3b3ebd0cd207db3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-24 20:51:54 -0700

    Google Interview Prep - LinkedLists - Has Cycle

---
+++ GoogleInterviewPrep/src/LinkedLists/HasCycle.java
@@ -0,0 +1,19 @@
+package LinkedLists;
+
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(slow != fast) {
+            if(fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+    
+}
