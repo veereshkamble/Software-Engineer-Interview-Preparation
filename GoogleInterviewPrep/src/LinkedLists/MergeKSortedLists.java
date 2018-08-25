c76c9ed556a6789813030f52dfe5db6cdb1a8062
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-24 22:07:38 -0700

    Google Interview Prep - LinkedLists - Merge K Sorted Lists

---
+++ GoogleInterviewPrep/src/LinkedLists/MergeKSortedLists.java
@@ -0,0 +1,12 @@
+package LinkedLists;
+
+public class MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+
+ 
+}
