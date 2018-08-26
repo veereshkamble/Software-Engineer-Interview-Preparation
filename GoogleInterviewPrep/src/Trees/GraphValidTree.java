d386b69838b3557e9a2ece62095306f1775c2cf7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-25 21:54:41 -0700

    Google Interview Prep - Trees - Graph Valid Trees - validTrees()

--- GoogleInterviewPrep/src/Trees/GraphValidTree.java
+++ GoogleInterviewPrep/src/Trees/GraphValidTree.java
@@ -1,4 +1,25 @@
 package Trees;
 
 public class GraphValidTree {
+
+    public boolean validTree(int n, int[][] edges) {
+        // initialize n isolated islands
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+
+            // union
+            nums[x] = y;
+        }
+
+        return edges.length == n - 1;
+    }
 }
