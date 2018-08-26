3218e483b94da6816c74a0ea38ade137796ff3e3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-26 00:22:19 -0700

    Google Interview Prep - Trees - InorderSuccessorBST TreeNode & successor()

--- GoogleInterviewPrep/src/Trees/InorderSuccessorBST.java
+++ GoogleInterviewPrep/src/Trees/InorderSuccessorBST.java
@@ -15,4 +15,6 @@ public class InorderSuccessorBST {
         }
         return ret;
     }
+
+
 }
---
+++ GoogleInterviewPrep/src/Trees/TreeNode.java
@@ -0,0 +1,10 @@
+package Trees;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
