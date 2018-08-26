14afeabe3fce3da5768a2538afcf39a9a5c02bd3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-26 16:19:03 -0700

    Google Interview Prep - Trees - Validate Division

---
+++ GoogleInterviewPrep/src/Trees/EvaluateDivision.java
@@ -0,0 +1,28 @@
+package Trees;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class EvaluateDivision {
+
+    public static double[] calcEquation(String[][] equations, double[] values, String[][] query) {
+        Map<String, Map<String, Double>> numMap = new HashMap<>();
+        int i = 0;
+        for(String[] str : equations) {
+            insertPairs(numMap, str[0], str[1], values[i]);
+            insertPairs(numMap, str[1], str[0], 1.0/values[i]);
+            i++;
+        }
+
+        double[] res = new double[query.length];
+        i = 0;
+        for(String[] q: query) {
+            Double resObj = handleQuery(q[0], q[1], numMap, new HashSet<>());
+            res[i++] = (resObj != null) ? resObj : -1.0;
+        }
+        return res;
+    }
+        
+}
