38c536b6a93b46395b30f580597ad42ed5f80328
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-27 15:23:05 -0700

    Google Interview Prep - Trees - Closeset Binary Search Tree Value Recursive

--- GoogleInterviewPrep/src/Trees/losestBinarySearchTreeValueRecursive.java
+++ GoogleInterviewPrep/src/Trees/losestBinarySearchTreeValueRecursive.java
@@ -2,5 +2,15 @@ package Trees;
 
 public class losestBinarySearchTreeValueRecursive {
 
-    
+    public int closestValue(TreeNode root, double target) {
+        return closest(root, target, root.val);
+    }
+
+    private int closest(TreeNode node, double target, int val) {
+        if (node == null) return val;
+        if (Math.abs(node.val - target) < Math.abs(val - target)) val = node.val;
+        if (node.val < target) val = closest(node.right, target, val);
+        else if (node.val > target) val = closest(node.left, target, val);
+        return val;
+    }
 }
