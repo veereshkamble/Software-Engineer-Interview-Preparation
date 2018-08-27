b409950eb1a7b76f1e59fbacf3df15d57acaf160
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-27 15:22:03 -0700

    Google Interview Prep - Trees - Closeset Binary Search Tree Value

--- GoogleInterviewPrep/src/Trees/ClosestBinarySearchTreeValue.java
+++ GoogleInterviewPrep/src/Trees/ClosestBinarySearchTreeValue.java
@@ -1,4 +1,25 @@
 package Trees;
 
 public class ClosestBinarySearchTreeValue {
+
+    public int closestValue(TreeNode root, double target) {
+        double closest = Integer.MAX_VALUE;
+        int value = 0;
+        TreeNode current = root;
+        while (current != null) {
+            if (closest > Math.abs(current.val-target)) {
+                closest = Math.abs(current.val-target);
+                value = current.val;
+            }
+
+            if (current.val < target) {
+                current = current.right;
+            } else if (current.val > target) {
+                current = current.left;
+            } else {
+                break;
+            }
+        }
+        return value;
+    }
 }
