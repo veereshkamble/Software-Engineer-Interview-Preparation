e8c8f546640fca068eea7dae5bd296abbd53141c
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-27 22:16:58 -0700

    Google Interview Prep - Recursion - Word Squares

--- GoogleInterviewPrep/src/Recursion/WordSquares.java
+++ GoogleInterviewPrep/src/Recursion/WordSquares.java
@@ -1,4 +1,19 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordSquares {
+
+    class TrieNode {
+        List<String> startWith;
+        TrieNode[] children;
+
+        TrieNode() {
+            startWith = new ArrayList<>();
+            children = new TrieNode[26];
+        }
+    }
+
+   
 }
