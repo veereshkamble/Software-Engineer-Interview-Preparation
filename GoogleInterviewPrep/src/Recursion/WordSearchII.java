62f38e23e78f7e8b061aa3fe254e4562e9f66f86
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-27 22:40:57 -0700

    Google Interview Prep - Recursion - Word Search - findWords()

--- GoogleInterviewPrep/src/Recursion/WordSearchII.java
+++ GoogleInterviewPrep/src/Recursion/WordSearchII.java
@@ -1,4 +1,17 @@
 package Recursion;
 
 public class WordSearchII {
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs (board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+   
 }
