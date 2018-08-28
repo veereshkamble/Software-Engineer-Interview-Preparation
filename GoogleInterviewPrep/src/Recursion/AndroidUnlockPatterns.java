bcdf391d30941e68ab96a40308c57f151ff4ae3e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-28 15:26:35 -0700

    Google Interview Prep - Recursion - Android Unlock Patterns - numberOfPatterns()

---
+++ GoogleInterviewPrep/src/Recursion/AndroidUnlockPatterns.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class AndroidUnlockPatterns {
+
+    private int[][] jumps;
+    private boolean[] visited;
+
+    public int numberOfPatterns(int m, int n) {
+        jumps = new int[10][10];
+        jumps[1][3] = jumps[3][1] = 2;
+        jumps[4][6] = jumps[6][4] = 5;
+        jumps[7][9] = jumps[9][7] = 8;
+        jumps[1][7] = jumps[7][1] = 4;
+        jumps[2][8] = jumps[8][2] = jumps[1][9] = jumps[9][1] = 5;
+        jumps[9][3] = jumps[3][9] = 6;
+        jumps[3][7] = jumps[7][3] = 5;
+        visited = new boolean[10];
+        int count = 0;
+        count += dfs(1, 1, 0, m, n) * 4;//1,3,7,9 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
+        count += dfs(2, 1, 0, m, n) * 4;//2,4,6,8 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
+        count += dfs(5, 1, 0, m, n);
+        return count;
+    }
+
+    
+
+}
