9508945e4867d726ac4a8575e98e6c1696aed95b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-28 13:14:31 -0700

    Google Interview Prep - Recursion - Strobogrammatic Number II

--- GoogleInterviewPrep/src/Recursion/StrobogrammaticNumberII.java
+++ GoogleInterviewPrep/src/Recursion/StrobogrammaticNumberII.java
@@ -1,4 +1,23 @@
 package Recursion;
 
 public class StrobogrammaticNumberII {
+
+    public List<String> findStrobogrammatic(int n) {
+        List<String> one = Arrays.asList("0", "1", "8"), two = Arrays.asList(""), r = two;
+        if(n%2 == 1)
+            r = one;
+        for(int i=(n%2)+2; i<=n; i+=2){
+            List<String> newList = new ArrayList<>();
+            for(String str : r){
+                if(i != n)
+                    newList.add("0" + str + "0");
+                newList.add("1" + str + "1");
+                newList.add("6" + str + "9");
+                newList.add("8" + str + "8");
+                newList.add("9" + str + "6");
+            }
+            r = newList;
+        }
+        return r;
+    }
 }
