8e7e7659bdb2a7ef33ec46c6095a7f836c7452ba
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-30 17:42:48 -0700

    Google Interview Prep - Design - Design Tic-Tac-Toe - move()

--- GoogleInterviewPrep/src/Design/TicTacToe.java
+++ GoogleInterviewPrep/src/Design/TicTacToe.java
@@ -20,7 +20,32 @@ public class TicTacToe {
      0: No one wins.
      1: Player 1 wins.
      2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        int toAdd = player == 1 ? 1 : -1;
 
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        if (row == col)
+        {
+            diagonal += toAdd;
+        }
+
+        if (col == (cols.length - row - 1))
+        {
+            antiDiagonal += toAdd;
+        }
+
+        int size = rows.length;
+        if (Math.abs(rows[row]) == size ||
+                Math.abs(cols[col]) == size ||
+                Math.abs(diagonal) == size  ||
+                Math.abs(antiDiagonal) == size)
+        {
+            return player;
+        }
+
+        return 0;
+    }
 }
 
 /**
