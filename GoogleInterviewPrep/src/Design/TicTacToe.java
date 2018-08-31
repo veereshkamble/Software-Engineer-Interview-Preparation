d826591cf3b013d79c08659f175c6554ef2cd4c9
Veeresh Kamble <veereshkamble7@gmail.com>
2018-08-30 17:28:59 -0700

    Google Interview Prep - Design - Design Tic-Tac-Toe - declaring data structures to use

--- GoogleInterviewPrep/src/Design/TicTacToe.java
+++ GoogleInterviewPrep/src/Design/TicTacToe.java
@@ -1,6 +1,27 @@
 package Design;
 
 public class TicTacToe {
+    private int[] rows;
+    private int[] cols;
+    private int diagonal;
+    private int antiDiagonal;
 
+    /** Initialize your data structure here. */
     
+
+    /** Player {player} makes a move at ({row}, {col}).
+     @param row The row of the board.
+     @param col The column of the board.
+     @param player The player, can be either 1 or 2.
+     @return The current winning condition, can be either:
+     0: No one wins.
+     1: Player 1 wins.
+     2: Player 2 wins. */
+
 }
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
