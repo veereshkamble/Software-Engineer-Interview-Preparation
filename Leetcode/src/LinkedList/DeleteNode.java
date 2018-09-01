1d8b2df4326e26bbb072ad59cc0b5fd834612333
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-01 15:49:59 -0700

    LeetCode - Easy Collection - Delete a Node in a Linked List - deleteNode()

--- Leetcode/src/LinkedList/DeleteNode.java
+++ Leetcode/src/LinkedList/DeleteNode.java
@@ -1,4 +1,20 @@
 package LinkedList;
 
 public class DeleteNode {
+
+    public void deleteNode(Node node) {
+
+        Node cur = node;
+        while(cur.next != null) {
+            Node next = cur.next;
+
+            cur.val = cur.next.val;
+            cur.next = cur.next.next;
+
+            cur = next;
+        }
+
+
+
+    }
 }
