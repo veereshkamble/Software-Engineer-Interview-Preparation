4d762e93824f8fe9f2025c17cb40966305ad11a5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-01 16:17:46 -0700

    LeetCode - Easy Collection - Sorting And Searching - Merge Sorted Array()

--- Leetcode/src/SortingAndSearching/MergeSortedArray.java
+++ Leetcode/src/SortingAndSearching/MergeSortedArray.java
@@ -2,5 +2,19 @@ package SortingAndSearching;
 
 public class MergeSortedArray {
 
-    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = nums1.length - 1;
+        int j = m - 1;
+        int k = n - 1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[j] >= nums2[k]) {
+                nums1[i] = nums1[j];
+                j--;
+            } else {
+                nums1[i] = nums2[k];
+                k--;
+            }
+            i--;
+        }
+    }
 }
