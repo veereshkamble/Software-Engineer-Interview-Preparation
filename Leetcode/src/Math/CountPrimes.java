33535067086ae60be1f86a9a19385fdc0dba2d71
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-02 22:59:30 -0700

    LeetCode - Easy Collection - Math - Count Primes

--- Leetcode/src/Math/CountPrimes.java
+++ Leetcode/src/Math/CountPrimes.java
@@ -1,4 +1,29 @@
 package Math;
 
 public class CountPrimes {
+
+    public int countPrimes(int n) {
+
+        if(n == 0) {
+            return 0;
+        }
+
+
+
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            boolean isPrime = true;
+
+            for(int j = 2; j <= i/2; j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
