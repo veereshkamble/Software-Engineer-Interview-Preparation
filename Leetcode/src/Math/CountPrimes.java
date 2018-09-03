3a9d008ce1e17c9e0b84f1ca42f59a059466985e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-03 14:31:26 -0700

    LeetCode - Easy Collection - Math - Count Primes Dynamic Programming

--- Leetcode/src/Math/CountPrimes.java
+++ Leetcode/src/Math/CountPrimes.java
@@ -23,7 +23,23 @@ public class CountPrimes {
         }
         return true;
     }
-    
+
+
+    public int countPrimesDynamic(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
         int n = 10;
         CountPrimes countPrimes = new CountPrimes();
