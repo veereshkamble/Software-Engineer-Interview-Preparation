79f4bbd86b11208894e50ea436142a26f0ccaa5e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-03 16:09:45 -0700

    LeetCode - Easy Collection - Math - Power of three

--- Leetcode/src/Math/PowerOfThree.java
+++ Leetcode/src/Math/PowerOfThree.java
@@ -1,4 +1,20 @@
 package Math;
 
 public class PowerOfThree {
+
+    public boolean isPowerOfThree(int n) {
+        if (n < 3 && n != 1) {
+            return false;
+        }
+        while (n != 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+
+    }
+
+    
 }
