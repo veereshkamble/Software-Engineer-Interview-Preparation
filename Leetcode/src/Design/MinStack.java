bd2bbf8a7f77afcf60d5fc352ee40a0e2bc41e4f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-02 22:00:24 -0700

    LeetCode - Easy Collection - Design - Min Stack()

--- Leetcode/src/Design/MinStack.java
+++ Leetcode/src/Design/MinStack.java
@@ -1,4 +1,38 @@
 package Design;
 
+import java.util.Stack;
+
 public class MinStack {
+
+    int min;
+    Stack<Integer> stack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        if(x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+
+    }
+
+    public void pop() {
+        if(stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
 }
