61514f408fbd74ddde5b3b87028abd776a3978ca
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-02 20:21:20 -0700

    LeetCode - Easy Collection - Sorting and Searching - FirstBadVersion()

--- Leetcode/src/SortingAndSearching/FirstBadVersion.java
+++ Leetcode/src/SortingAndSearching/FirstBadVersion.java
@@ -1,4 +1,14 @@
 package SortingAndSearching;
 
 public class FirstBadVersion {
+
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end-start) / 2;
+            if (!isBadVersion(mid)) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }
 }
