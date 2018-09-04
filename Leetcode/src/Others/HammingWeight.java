a975843bb9e045bfa04811fdaba6f866959b7f95
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-03 20:29:22 -0700

    LeetCode - Easy Collection - Others - Hamming Weight

---
+++ Leetcode/src/Others/HammingWeight.java
@@ -0,0 +1,13 @@
+package Others;
+
+public class HammingWeight {
+
+    public int hammingWeight(int n) {
+        int bits = 0;
+        while (n != 0) {
+            bits++;
+            n &= (n - 1);
+        }
+        return bits;
+    }
+}
