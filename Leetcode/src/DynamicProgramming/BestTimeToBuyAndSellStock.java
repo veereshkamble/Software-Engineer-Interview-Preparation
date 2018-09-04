9f92c5cd16a47ae90b4883fe1a500691ec7d76e3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-04 16:14:31 -0700

    Leetcode - Easy Collection - Dynamic Programming - Best Time to Buy and Sell Stock

--- Leetcode/src/DynamicProgramming/BestTimeToBuyAndSellStock.java
+++ Leetcode/src/DynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -2,5 +2,20 @@ package DynamicProgramming;
 
 public class BestTimeToBuyAndSellStock {
 
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int buy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - buy);
+            }
+        }
+        return maxProfit;
+    }
     
 }
