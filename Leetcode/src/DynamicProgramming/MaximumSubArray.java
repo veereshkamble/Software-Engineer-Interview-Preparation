33871400f305a9c55f73b1ec1dab8d04059e6e4d
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-04 18:25:58 -0700

    Leetcode - Easy Collection - Dynamic Programming - Maximum Subarray

--- Leetcode/src/DynamicProgramming/MaximumSubArray.java
+++ Leetcode/src/DynamicProgramming/MaximumSubArray.java
@@ -1,4 +1,14 @@
 package DynamicProgramming;
 
 public class MaximumSubArray {
+
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+        }
+        return maxSoFar;
+    }
 }
