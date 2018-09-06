4c15e0a58ea8840f2f743f23ff56d1af961a8141
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-06 16:28:30 -0700

    Interview Questions - Grammarly Video Interview - Longest Consecutive Sequence

--- Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
+++ Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
@@ -1,5 +1,15 @@
 package Grammarly;
 
 public class LongestConsecutiveSequence {
+
+    private boolean arrayContains(int[] nums, int num) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == num) {
+                return true;
+            }
+        }
+        return  false;
+    }
+
     
 }
