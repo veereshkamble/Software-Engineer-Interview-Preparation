e9caca0c30715237e05bf6d91142a16033e31a5a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-06 17:29:45 -0700

    Interview Questions - Grammarly Video Interview - Longest Consecutive Sequence

--- Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
+++ Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package Grammarly;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
 
@@ -46,6 +48,30 @@ public class LongestConsecutiveSequence {
                     longestStreak = Math.max(longestStreak, currentStreak);
                     currentStreak = 1;
                 }
+            }
+        }
+        return longestStreak;
+    }
+
+    public static int longestSubsequenceHashSet(int[] nums) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+
+        for(int num : nums) {
+            hashSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for(int num : nums) {
+            if(!hashSet.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (hashSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
             }
         }
         return longestStreak;
