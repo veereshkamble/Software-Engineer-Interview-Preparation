acab9fa6b21b84be818b39d5ae919de38c7aa441
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-06 17:30:50 -0700

    Interview Questions - Grammarly Video Interview - Longest Consecutive Sequence

--- Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
+++ Interview Questions/src/Grammarly/LongestConsecutiveSequence.java
@@ -82,6 +82,7 @@ public class LongestConsecutiveSequence {
         int[] nums = new int[] {100, 4, 200, 1, 3, 2};
         System.out.println(longestSequenceBruteForce(nums));
         System.out.println(longestSequqenceSorting(nums));
+        System.out.println(longestSubsequenceHashSet(nums));
     }
 
 
