005b368767fd91e0274355b3098869a585eb07f0
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-06 17:32:51 -0700

    LeetCode - HashSet - Longest Consectuive Sequence

--- Leetcode/src/HashTables/LongestConsecutiveSequence.java
+++ Leetcode/src/HashTables/LongestConsecutiveSequence.java
@@ -1,4 +1,15 @@
 package HashTables;
 
 public class LongestConsecutiveSequence {
+
+    private static boolean arrayContains(int[] nums, int num) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == num) {
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    
 }
