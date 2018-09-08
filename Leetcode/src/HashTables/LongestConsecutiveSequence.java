8ce10600df6b50c90ce381b0431f81f87f0461af
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-07 23:49:38 -0700

    LeetCode - HashSet - Longest Consectuive Sequence

--- Leetcode/src/HashTables/LongestConsecutiveSequence.java
+++ Leetcode/src/HashTables/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package HashTables;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
 
@@ -50,4 +52,29 @@ public class LongestConsecutiveSequence {
         }
         return longestStreak;
     }
+
+    public static int longestSubsequenceHashSet(int[] nums) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+
+        for(int num : nums) {
+            hashSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for(int num : nums) {
+            if(!hashSet.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (hashSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+    
 }
