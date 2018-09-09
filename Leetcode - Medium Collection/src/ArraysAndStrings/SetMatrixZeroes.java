db71a3c2df8039fbddb0d713f419066bcd6e0c8e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-09 00:08:15 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Set Matrix

--- Leetcode - Medium Collection/src/ArraysAndStrings/SetMatrixZeroes.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/SetMatrixZeroes.java
@@ -70,4 +70,20 @@ public class SetMatrixZeroes {
             matrix[row][j] = 0;
         }
     }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {{0,1,2,0},
+                {3,4,5,2},
+                {1,3,1,5}};
+
+        SetMatrixZeroes setMatrixZeroes = new SetMatrixZeroes();
+        setMatrixZeroes.setZeroes(matrix);
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j <matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
