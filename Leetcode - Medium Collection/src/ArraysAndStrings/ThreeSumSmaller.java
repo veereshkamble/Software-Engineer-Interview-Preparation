e33b7a3c7539ce85424f87724d5d8402df94f1a2
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-08 23:49:50 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Three Sum

---
+++ Leetcode - Medium Collection/src/ArraysAndStrings/ThreeSumSmaller.java
@@ -0,0 +1,29 @@
+package ArraysAndStrings;
+
+public class ThreeSumSmaller {
+
+    static int arr[] = new int[]{5, 1, 3, 4, 7};
+
+    static int countTriplets(int n, int sum)
+    {
+        // Initialize result
+        int ans = 0;
+
+        // Fix the first element as A[i]
+        for (int i = 0; i < n-2; i++)
+        {
+            // Fix the second element as A[j]
+            for (int j = i+1; j < n-1; j++)
+            {
+                // Now look for the third number
+                for (int k = j+1; k < n; k++)
+                    if (arr[i] + arr[j] + arr[k] < sum)
+                        ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    
+}
