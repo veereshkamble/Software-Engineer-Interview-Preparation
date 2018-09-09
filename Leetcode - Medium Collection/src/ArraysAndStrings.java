fab1049570585428fdd316552201d17b85c2efc3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-08 22:53:35 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Three Sum

---
+++ Leetcode - Medium Collection/src/ArraysAndStrings.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraysAndStrings {
+
+    public List<List<Integer>> threeSumThreeLoops(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        int n = nums.length;
+
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                for(int k = j + 1; k < n; k++) {
+                    if(nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        Collections.sort(list);
+
+                        if(!result.contains(list)) {
+                            result.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    
+}
