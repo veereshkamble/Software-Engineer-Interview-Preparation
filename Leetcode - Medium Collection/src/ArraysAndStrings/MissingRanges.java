94f1bd111fc0a9bdbca8995f33140b311880ff40
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-09 21:56:37 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Missing Ranges

--- Leetcode - Medium Collection/src/ArraysAndStrings/MissingRanges.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/MissingRanges.java
@@ -5,36 +5,47 @@ import java.util.List;
 
 public class MissingRanges {
 
-    public List<String> findMissingRanges(int[] a, int lo, int hi) {
-        List<String> res = new ArrayList<String>();
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> res = new ArrayList<>();
 
-        // the next number we need to find
-        int next = lo;
-
-        for (int i = 0; i < a.length; i++) {
-            // not within the range yet
-            if (a[i] < next) continue;
-
-            // continue to find the next one
-            if (a[i] == next) {
-                next++;
-                continue;
-            }
-
-            // get the missing range string format
-            res.add(getRange(next, a[i] - 1));
-
-            // now we need to find the next number
-            next = a[i] + 1;
+        if(nums.length == 0) {
+            res.add(generateRange(lower, upper));
+            return res;
         }
 
-        // do a final check
-        if (next <= hi) res.add(getRange(next, hi));
+        if(lower == Integer.MAX_VALUE) {
+            return res;
+        }
+
+        int next = lower;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] < next)
+                continue;
+
+            if(nums[i] == next) {
+                next = next+1;
+                continue;
+            }
+            res.add(generateRange(next, nums[i]-1));
+
+            //Overflow : no need to update and check, just return
+            if(nums[i] == upper) {
+                return res;
+            }
+            next = nums[i]+1;
+        }
+
+        //check if upper didn't overlap in the nums array
+        if(next <= upper)
+            res.add(generateRange(next, upper));
 
         return res;
     }
 
-    String getRange(int n1, int n2) {
-        return (n1 == n2) ? String.valueOf(n1) : String.format("%d->%d", n1, n2);
+    private String generateRange(long start, long end) {
+        if(start == end)
+            return ""+start;
+        else
+            return start+"->"+end;
     }
 }
