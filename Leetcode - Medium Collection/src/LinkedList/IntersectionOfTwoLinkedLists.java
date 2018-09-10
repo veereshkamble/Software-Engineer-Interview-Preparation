73eb70d957c238e43de3b2d5c23facb0507b9322
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-10 14:36:45 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Linked Lists - Intersection of Two Linked Lists

---
+++ Leetcode - Medium Collection/src/LinkedList/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+public class IntersectionOfTwoLinkedLists {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = length(headA);
+        int lengthB = length(headB);
+
+        while(lengthA > lengthB) {
+            headA = headA.next;
+            lengthA--;
+        }
+
+        while(lengthA < lengthB) {
+            headB = headB.next;
+            lengthB--;
+        }
+
+        while(headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
+    public int length(ListNode node) {
+        int length = 0;
+        while(node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+}
