d7390bbc851cd93e981ed6de5351f1df1e1486bf
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-09 19:24:31 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Longest Substring without repeating characters

--- Leetcode - Medium Collection/src/ArraysAndStrings/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,30 @@
 package ArraysAndStrings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int begin = 0, end = 0, counter = 0, len = 0;
+
+        while (end < s.length()) {
+            // > 0 means repeating character
+            //if(map[s.charAt(end++)]-- > 0) counter++;
+            char c = s.charAt(end);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            if(map.get(c) > 1) counter++;
+            end++;
+
+            while (counter > 0) {
+                //if (map[s.charAt(begin++)]-- > 1) counter--;
+                char charTemp = s.charAt(begin);
+                if (map.get(charTemp) > 1) counter--;
+                map.put(charTemp, map.get(charTemp)-1);
+                begin++;
+            }
+            len = Math.max(len, end - begin);
+        }
+        return len;
+    }
 }
