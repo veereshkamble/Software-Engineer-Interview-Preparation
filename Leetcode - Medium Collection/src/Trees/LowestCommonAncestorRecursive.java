adf05392bbfbd982954b043c5d0e4a41fce551fc
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-10 22:10:43 -0700

    SAP Interview Prep - LeetCode - Top Interview Questions - Medium Collection - Trees - LowestCommonAncestorRecursive

---
+++ Leetcode - Medium Collection/src/Trees/LowestCommonAncestorRecursive.java
@@ -0,0 +1,12 @@
+package Trees;
+
+//Java implementation to find lowest common ancestor of
+// n1 and n2 using one traversal of binary tree
+
+/* Class containing left and right child of current
+ node and key value*/
+
+public class LowestCommonAncestorRecursive
+{
+    
+}
