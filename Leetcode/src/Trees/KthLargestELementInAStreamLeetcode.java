591c96d5378985e59e82bb886a21058d4eb47bd5
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-11 17:52:30 -0700

    SAP Interview Prep - LeetCode - The day before the interview - Just reviewing - Kth Largest Element in a Stream LeetCode

--- Leetcode/src/Trees/KthLargestELementInAStreamLeetcode.java
+++ Leetcode/src/Trees/KthLargestELementInAStreamLeetcode.java
@@ -1,4 +1,28 @@
 package Trees;
 
 public class KthLargestELementInAStreamLeetcode {
+
+
+    class KthLargest {
+        // insert a node into the BST
+        private Node insertNode(Node root, int num) {
+            if (root == null) {
+                return new Node(num, 1);
+            }
+            if (root.val < num) {
+                root.right = insertNode(root.right, num);
+            } else {
+                root.left = insertNode(root.left, num);
+            }
+            root.cnt++;
+            return root;
+        }
+
+  
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
 }
