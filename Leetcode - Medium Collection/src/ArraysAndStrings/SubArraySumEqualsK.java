1d281d3d19702708ae1153c4fa383e9087af472f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-13 23:57:32 -0700

    LeetCOde Medium Collection - Sub Array Sum Equals K

--- Leetcode - Medium Collection/src/ArraysAndStrings/SubArraySumEqualsK.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/SubArraySumEqualsK.java
@@ -15,4 +15,19 @@ public class SubArraySumEqualsK {
         }
         return count;
     }
+
+    public int subarraySumCumulativeSum(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k)
+                    count++;
+            }
+        }
+        return count;
+    }
 }
