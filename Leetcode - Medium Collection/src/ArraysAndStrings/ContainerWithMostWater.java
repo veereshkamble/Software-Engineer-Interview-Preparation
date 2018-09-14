102d25c694ec76c523c6185a532918950fdbc9dc
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-13 23:51:59 -0700

    LeetCOde Medium Collection - Container with Most Water

--- Leetcode - Medium Collection/src/ArraysAndStrings/ContainerWithMostWater.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/ContainerWithMostWater.java
@@ -9,4 +9,16 @@ public class ContainerWithMostWater {
                 maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
         return maxarea;
     }
+
+    public int maxAreaTwoPointer(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
 }
