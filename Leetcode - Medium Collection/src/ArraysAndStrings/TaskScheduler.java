3fae072c5bd580d16f6ab18c65f5790863ff688f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-13 23:54:46 -0700

    LeetCOde Medium Collection - Task Scheduler

--- Leetcode - Medium Collection/src/ArraysAndStrings/TaskScheduler.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/TaskScheduler.java
@@ -1,5 +1,7 @@
 package ArraysAndStrings;
 
+import java.util.*;
+
 public class TaskScheduler   {
 
     public int leastInterval(char[] tasks, int n) {
@@ -19,6 +21,37 @@ public class TaskScheduler   {
                 i++;
             }
             Arrays.sort(map);
+        }
+        return time;
+    }
+
+    public int leastIntervalPriorityQueue(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        PriorityQueue< Integer > queue = new PriorityQueue < > (26, Collections.reverseOrder());
+        for (int f: map) {
+            if (f > 0)
+                queue.add(f);
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int i = 0;
+            List< Integer > temp = new ArrayList< >();
+            while (i <= n) {
+                if (!queue.isEmpty()) {
+                    if (queue.peek() > 1)
+                        temp.add(queue.poll() - 1);
+                    else
+                        queue.poll();
+                }
+                time++;
+                if (queue.isEmpty() && temp.size() == 0)
+                    break;
+                i++;
+            }
+            for (int l: temp)
+                queue.add(l);
         }
         return time;
     }
