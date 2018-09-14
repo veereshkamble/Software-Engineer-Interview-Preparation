d6fde63e09133d512baf4b3228ba4216d88bbf0a
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-14 00:00:17 -0700

    LeetCOde Medium Collection - Sub Array Sum Equals K

--- Leetcode - Medium Collection/src/ArraysAndStrings/SubArraySumEqualsK.java
+++ Leetcode - Medium Collection/src/ArraysAndStrings/SubArraySumEqualsK.java
@@ -1,5 +1,7 @@
 package ArraysAndStrings;
 
+import java.util.HashMap;
+
 public class SubArraySumEqualsK {
 
     public int subarraySumBruteForce(int[] nums, int k) {
@@ -27,6 +29,19 @@ public class SubArraySumEqualsK {
                 if (sum[end] - sum[start] == k)
                     count++;
             }
+        }
+        return count;
+    }
+
+    public int subarraySumHashMap(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap< Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
