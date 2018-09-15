0f2a2dbfe70a6c6dff09358160edb3fd15d0ec30
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-15 12:35:53 -0700

    SAP Onsite Interview Prep - LeetCode - Interval based questions - Meeting Rooms I

--- Leetcode - Medium Collection/src/Intervals/MeetingRoomsI.java
+++ Leetcode - Medium Collection/src/Intervals/MeetingRoomsI.java
@@ -1,4 +1,19 @@
 package Intervals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MeetingRoomsI {
+
+    public boolean canAttendMeetingsSorting(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval i1, Interval i2) {
+                return i1.start - i2.start;
+            }
+        });
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) return false;
+        }
+        return true;
+    }
 }
