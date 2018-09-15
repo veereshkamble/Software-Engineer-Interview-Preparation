1a82ac33001f8c740c1e2c521adf9a31091e25f3
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-14 22:06:46 -0700

    SAP Onsite Interview Prep - LeetCode - Interval based questions

---
+++ Leetcode - Medium Collection/src/Intervals/Interval.java
@@ -0,0 +1,17 @@
+package Intervals;
+
+public class Interval {
+
+    int start;
+    int end;
+
+    public Interval() {
+        this.start = 0;
+        this.end = 0;
+    }
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
