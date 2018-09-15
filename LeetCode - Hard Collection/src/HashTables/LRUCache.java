6a6249fb3c3f79820128e77b38e38347c8016dcc
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-14 19:22:00 -0700

    SAP Onsite Interview Prep - LeetCode Hard Collection - Designing LRU Cache

--- LeetCode - Hard Collection/src/HashTables/LRUCache.java
+++ LeetCode - Hard Collection/src/HashTables/LRUCache.java
@@ -1,4 +1,23 @@
 package HashTables;
 
 public class LRUCache {
+    private class Node{
+        int key, value;
+        Node prev, next;
+        Node(int k, int v){
+            this.key = k;
+            this.value = v;
+        }
+        Node(){
+            this(0, 0);
+        }
+    }
+
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
