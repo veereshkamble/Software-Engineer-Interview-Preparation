18935489ad69f7b193d5856df4f3981b991dc54f
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-15 17:23:41 -0700

    SAP Onsite Interview Prep - LeetCode - Interval based questions - Meeting Rooms II

--- Leetcode - Medium Collection/src/Intervals/MeetingRoomsII.java
+++ Leetcode - Medium Collection/src/Intervals/MeetingRoomsII.java
@@ -1,4 +1,54 @@
 package Intervals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MeetingRoomsII {
+
+    public int minMeetingRooms(Interval[] intervals) {
+
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        // Min heap
+        PriorityQueue<Integer> allocator =
+                new PriorityQueue<Integer>(
+                        intervals.length,
+                        new Comparator<Integer>() {
+                            public int compare(Integer a, Integer b) {
+                                return a - b;
+                            }
+                        });
+
+        // Sort the intervals by start time
+        Arrays.sort(
+                intervals,
+                new Comparator<Interval>() {
+                    public int compare(Interval a, Interval b) {
+                        return a.start - b.start;
+                    }
+                });
+
+        // Add the first meeting
+        allocator.add(intervals[0].end);
+
+        // Iterate over remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+
+            // If the room due to free up the earliest is free, assign that room to this meeting.
+            if (intervals[i].start >= allocator.peek()) {
+                allocator.poll();
+            }
+
+            // If a new room is to be assigned, then also we add to the heap,
+            // If an old room is allocated, then also we have to add to the heap with updated end time.
+            allocator.add(intervals[i].end);
+        }
+
+        // The size of the heap tells us the minimum rooms required for all the meetings.
+        return allocator.size();
+    }
 }
