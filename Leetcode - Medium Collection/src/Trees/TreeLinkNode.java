bf3990350bfa15e3073a625545047ad901292c0d
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-20 17:58:37 -0700

    Coding Practice - Leetcode - Medium Collection - Trees and Graphs - Populating next right pointer in each node

---
+++ Leetcode - Medium Collection/src/Trees/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package Trees;
+
+public class TreeLinkNode {
+
+    int val;
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+
+    public TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
