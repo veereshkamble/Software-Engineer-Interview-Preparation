8fb695ef424ba095f8c912f11fa954edfe601302
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-20 17:45:53 -0700

    Coding Practice - Leetcode - Medium Collection - Trees and Graphs - Binary Tree Inorder Traversal Iterative Approach

--- Leetcode - Medium Collection/src/Trees/BinaryTreeInorderTraversal.java
+++ Leetcode - Medium Collection/src/Trees/BinaryTreeInorderTraversal.java
@@ -1,7 +1,9 @@
 package Trees;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreeInorderTraversal {
 
@@ -13,5 +15,23 @@ public class BinaryTreeInorderTraversal {
             result.addAll(inorderTraversalRecursive(root.right));
         }
         return result;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<Integer>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {// Travel to each node's left child, till reach the left leaf
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop(); // Backtrack to higher level node A
+            res.add(cur.val);  // Add the node to the result list
+            cur = cur.right;   // Switch to A'right branch
+        }
+        return res;
     }
 }
