151989e69141b28b4dd028d513aad3bf45d8b3be
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-22 21:16:51 -0700

    Coding Practice - Leetcode - Medium Collection - Backtracing - PermutationsII

--- Leetcode - Medium Collection/src/Backtracking/PermutationsII.java
+++ Leetcode - Medium Collection/src/Backtracking/PermutationsII.java
@@ -1,6 +1,7 @@
 package Backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PermutationsII {
@@ -12,5 +13,18 @@ public class PermutationsII {
         return list;
     }
 
-    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
 }
