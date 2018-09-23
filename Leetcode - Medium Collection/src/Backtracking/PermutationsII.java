e6c57120fb8db1b0baaeeead6bd3bd2399bfc02e
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-22 21:16:22 -0700

    Coding Practice - Leetcode - Medium Collection - Backtracing - PermutationsII

--- Leetcode - Medium Collection/src/Backtracking/PermutationsII.java
+++ Leetcode - Medium Collection/src/Backtracking/PermutationsII.java
@@ -1,4 +1,16 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermutationsII {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+        return list;
+    }
+
+    
 }
