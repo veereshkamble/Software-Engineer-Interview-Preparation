b09f600d00ddccabb55949220153152fec9a1869
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-26 21:03:19 -0700

    Coding Practice - LeetCode - Easy Collection

---
+++ Leetcode/src/Practice/CombineTwoTables.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class CombineTwoTables {
+
+    /*# Write your MySQL query statement below
+    select FirstName, LastName, City, State
+    from Person left join Address
+    on Person.PersonId = Address.PersonId
+    ;*/
+}
