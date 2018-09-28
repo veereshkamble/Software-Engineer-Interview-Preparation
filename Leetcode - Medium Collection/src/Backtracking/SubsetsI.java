d866ea2e22fc3cdaf5eea70b472b1c8b49109e43
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-28 15:06:06 -0700

    Coding Practice - Leetcode - Medium Collection - Backtracking - Cisco Interview - Subsets

--- Leetcode - Medium Collection/src/Backtracking/SubsetsI.java
+++ Leetcode - Medium Collection/src/Backtracking/SubsetsI.java
@@ -1,4 +1,15 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubsetsI {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }   
 }
