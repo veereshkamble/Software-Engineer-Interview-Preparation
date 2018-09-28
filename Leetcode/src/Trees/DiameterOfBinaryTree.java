1b80c12d4481550e786171e8f8f01b5783c48985
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-27 22:29:06 -0700

    Coding Practice - LeetCode - Easy Collection - Diameter of binary tree

--- Leetcode/src/Trees/DiameterOfBinaryTree.java
+++ Leetcode/src/Trees/DiameterOfBinaryTree.java
@@ -1,4 +1,19 @@
 package Trees;
 
 public class DiameterOfBinaryTree {
+
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+    public int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L+R+1);
+        return Math.max(L, R) + 1;
+    }
+    
 }
