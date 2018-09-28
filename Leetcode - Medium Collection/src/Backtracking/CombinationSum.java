cfabfe1ac7536c6ca7d917ad092129f8d5251e20
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-28 16:55:24 -0700

    Coding Practice - Leetcode - Medium Collection - Backtracking - Cisco Interview - Combination Sum

--- Leetcode - Medium Collection/src/Backtracking/CombinationSum.java
+++ Leetcode - Medium Collection/src/Backtracking/CombinationSum.java
@@ -13,5 +13,17 @@ public class CombinationSum {
         return list;
     }
 
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
     
 }
