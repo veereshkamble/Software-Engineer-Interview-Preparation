5255da57420d1e61f440c2b4f6b02df52591759b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-29 15:48:41 -0700

    Coding Practice - Leetcode - Medium Collection - Sorting and Searching - Sort Colors

--- Leetcode - Medium Collection/src/SortingAndSearching/SortColors.java
+++ Leetcode - Medium Collection/src/SortingAndSearching/SortColors.java
@@ -16,5 +16,21 @@ public class SortColors {
         for(int i = 0; i < num2; ++i) A[num0+num1+i] = 2;
     }
 
-    
+    public void sortColorsOnePass(int[] nums) {
+        int n0 = -1, n1 = -1, n2 = -1;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 0)
+            {
+                nums[++n2] = 2; nums[++n1] = 1; nums[++n0] = 0;
+            }
+            else if (nums[i] == 1)
+            {
+                nums[++n2] = 2; nums[++n1] = 1;
+            }
+            else if (nums[i] == 2)
+            {
+                nums[++n2] = 2;
+            }
+        }
+    }
 }
