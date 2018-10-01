0ab6301fa3f50f3f47d573e0d6b81d04efca8932
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-30 18:50:33 -0700

    Coding Practice - Leetcode - Medium Collection - Cisco Interview - Sorting and Searching - Kth Largest Element

---
+++ Leetcode - Medium Collection/src/SortingAndSearching/KthLargestElement.java
@@ -0,0 +1,23 @@
+package SortingAndSearching;
+
+public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+
+        shuffle(nums);
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+}
