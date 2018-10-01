7f1d3b391a8ae27755cc566e9044f00973fff22b
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-30 18:51:52 -0700

    Coding Practice - Leetcode - Medium Collection - Cisco Interview - Sorting and Searching - Kth Largest Element - partition()

--- Leetcode - Medium Collection/src/SortingAndSearching/KthLargestElement.java
+++ Leetcode - Medium Collection/src/SortingAndSearching/KthLargestElement.java
@@ -35,4 +35,20 @@ public class KthLargestElement {
         a[i] = a[j];
         a[j] = tmp;
     }
+
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
 }
