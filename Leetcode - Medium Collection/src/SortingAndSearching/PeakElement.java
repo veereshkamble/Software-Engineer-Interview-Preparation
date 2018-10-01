6df5b7723d66a45dd9f8eda5c86ef38f17cf98f4
Veeresh Kamble <veereshkamble7@gmail.com>
2018-09-30 20:07:51 -0700

    Coding Practice - Leetcode - Medium Collection - Cisco Interview - Sorting and Searching - Peak Element - Iterative Binary Search

--- Leetcode - Medium Collection/src/SortingAndSearching/PeakElement.java
+++ Leetcode - Medium Collection/src/SortingAndSearching/PeakElement.java
@@ -22,5 +22,15 @@ public class PeakElement {
         return search(nums, mid + 1, r);
     }
 
-    
+    public int findPeakElementIterativeBinary(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
 }
