c1248a4343d906b9b93bf4739da3af20f29866bf
Veeresh Kamble <veereshkamble7@gmail.com>
2018-10-02 00:00:42 -0700

    Coding Practice - Leetcode - Easy Collection - Montonic Array

--- Leetcode/src/Practice/MonotonicArray.java
+++ Leetcode/src/Practice/MonotonicArray.java
@@ -17,4 +17,18 @@ public class MonotonicArray {
             if (A[i] < A[i+1]) return false;
         return true;
     }
+
+    public boolean isMonotonic(int[] A) {
+        int store = 0;
+        for (int i = 0; i < A.length - 1; ++i) {
+            int c = Integer.compare(A[i], A[i+1]);
+            if (c != 0) {
+                if (c != store && store != 0)
+                    return false;
+                store = c;
+            }
+        }
+
+        return true;
+    }
 }
