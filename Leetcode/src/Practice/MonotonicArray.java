1ae178bbe8de4690821f2ee5ed913624f11be7fe
Veeresh Kamble <veereshkamble7@gmail.com>
2018-10-02 00:00:16 -0700

    Coding Practice - Leetcode - Easy Collection - Montonic Array

--- Leetcode/src/Practice/MonotonicArray.java
+++ Leetcode/src/Practice/MonotonicArray.java
@@ -1,4 +1,20 @@
 package Practice;
 
 public class MonotonicArray {
+
+    public boolean isMonotonicTwoPass(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
 }
