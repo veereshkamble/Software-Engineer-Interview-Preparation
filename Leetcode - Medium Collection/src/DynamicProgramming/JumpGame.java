1b5df1682a005ccd1dc95d6fa4db9f1b294eddf7
Veeresh Kamble <veereshkamble7@gmail.com>
2018-10-08 23:56:06 -0700

    Coding Practice - LeetCode - Medium Collection - Dynamic Programming - Jump Game Backtracking

--- Leetcode - Medium Collection/src/DynamicProgramming/JumpGame.java
+++ Leetcode - Medium Collection/src/DynamicProgramming/JumpGame.java
@@ -1,4 +1,23 @@
 package DynamicProgramming;
 
 public class JumpGame {
+
+    public boolean canJumpFromPositionBacktracking(int position, int[] nums) {
+        if (position == nums.length - 1) {
+            return true;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPositionBacktracking(nextPosition, nums)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        return canJumpFromPositionBacktracking(0, nums);
+    }
 }
