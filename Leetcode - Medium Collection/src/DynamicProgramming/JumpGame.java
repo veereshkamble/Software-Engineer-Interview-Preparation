a04a06733a5e80bc7a4dc31d874591971bd308ce
Veeresh Kamble <veereshkamble7@gmail.com>
2018-10-08 23:58:21 -0700

    Coding Practice - LeetCode - Medium Collection - Dynamic Programming - Jump Game Greedy

--- Leetcode - Medium Collection/src/DynamicProgramming/JumpGame.java
+++ Leetcode - Medium Collection/src/DynamicProgramming/JumpGame.java
@@ -20,4 +20,14 @@ public class JumpGame {
     public boolean canJump(int[] nums) {
         return canJumpFromPositionBacktracking(0, nums);
     }
+
+    public boolean canJumpGreedy(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
 }
