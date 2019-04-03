05e1e5bcee5ce76dbf43a7b35606f94103446823
veereshkamble <veereshkamble7@gmail.com>
2019-04-02 21:49:46 -0700

    Buy Sell Stock II Peak Valley

--- Leetcode/src/ArraysAndStrings/BuyAndSellStockII.java
+++ Leetcode/src/ArraysAndStrings/BuyAndSellStockII.java
@@ -36,6 +36,23 @@ public class BuyAndSellStockII {
         return total;
     }
 
+    public int maxProfitPeakValley(int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+
     public static  void main(String[] args) {
         int[]  prices = new int[] {7, 1, 5, 3, 6, 4};
 
